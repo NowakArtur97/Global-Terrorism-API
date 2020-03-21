c8c5f235dd614fc3860bb26819c27644f081e117
NowakArtur97 <nowart97@gmail.com>
2020-03-21 18:43:44 +0100

    Add dependencies for reading large xlsx files and read cells values in console

--- src/main/java/com/NowakArtur97/GlobalTerrorismAPI/GlobalTerrorismApiApplication.java
+++ src/main/java/com/NowakArtur97/GlobalTerrorismAPI/GlobalTerrorismApiApplication.java
@@ -1,14 +1,24 @@
 package com.NowakArtur97.GlobalTerrorismAPI;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Iterator;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import lombok.extern.slf4j.Slf4j;
+import com.monitorjbl.xlsx.StreamingReader;
 
 @SpringBootApplication
-@Slf4j
 public class GlobalTerrorismApiApplication implements CommandLineRunner {
+
+	private final static String pathToFile = "C:\\Users\\Samsung\\Spring\\eclipse-workspace\\Projects\\GlobalTerrorismAPI\\src\\main\\resources\\data\\globalterrorismdb_0919dist.xlsx";
 
 	public static void main(String[] args) {
 		SpringApplication.run(GlobalTerrorismApiApplication.class, args);
@@ -17,6 +27,42 @@ public class GlobalTerrorismApiApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
-		log.info("STARTING THE APPLICATION");
+		File globalTerrorismFile = new File(pathToFile);
+
+		InputStream inputStream = new FileInputStream(globalTerrorismFile);
+
+		Workbook workbook = StreamingReader.builder().rowCacheSize(100).bufferSize(4096).open(inputStream);
+
+		Sheet sheet = workbook.getSheetAt(0);
+
+		Iterator<Row> rowIterator = sheet.rowIterator();
+
+		while (rowIterator.hasNext()) {
+
+			Row row = rowIterator.next();
+
+			Iterator<Cell> cellIterator = row.cellIterator();
+
+			while (cellIterator.hasNext()) {
+
+				Cell cell = cellIterator.next();
+
+				switch (cell.getCellType()) {
+				case STRING:
+					System.out.print(cell.getStringCellValue() + "\t");
+					break;
+				case NUMERIC:
+					System.out.print(cell.getNumericCellValue() + "\t");
+					break;
+				case BOOLEAN:
+					System.out.print(cell.getBooleanCellValue() + "\t");
+					break;
+				default:
+					System.out.print(cell.getStringCellValue());
+				}
+			}
+
+			System.out.println();
+		}
 	}
 }
