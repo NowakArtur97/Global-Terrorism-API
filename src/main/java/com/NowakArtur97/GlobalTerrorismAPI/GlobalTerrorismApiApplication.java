379367565af896779a442404a172afbdfe2efa09
NowakArtur97 <nowart97@gmail.com>
2020-03-21 12:08:17 +0100

    Make Main Class implement Command Line Runner

--- src/main/java/com/NowakArtur97/GlobalTerrorismAPI/GlobalTerrorismApiApplication.java
+++ src/main/java/com/NowakArtur97/GlobalTerrorismAPI/GlobalTerrorismApiApplication.java
@@ -1,13 +1,22 @@
 package com.NowakArtur97.GlobalTerrorismAPI;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootApplication
-public class GlobalTerrorismApiApplication {
+@Slf4j
+public class GlobalTerrorismApiApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GlobalTerrorismApiApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+		log.info("STARTING THE APPLICATION");
+	}
 }
