24cb4a5f5dd9eb585da3605d98bdf76dd4101c39
NowakArtur97 <nowart97@gmail.com>
2020-03-21 11:56:34 +0100

    Import Maven Project from Spring Initializr

---
+++ src/main/java/com/NowakArtur97/GlobalTerrorismAPI/GlobalTerrorismApiApplication.java
@@ -0,0 +1,13 @@
+package com.NowakArtur97.GlobalTerrorismAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobalTerrorismApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlobalTerrorismApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/NowakArtur97/GlobalTerrorismAPI/GlobalTerrorismApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NowakArtur97.GlobalTerrorismAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlobalTerrorismApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
