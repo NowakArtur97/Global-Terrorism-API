bd8acb5d7f542682dbf4a8b6fb8e7874089403d5
NowakArtur97 <nowart97@gmail.com>
2020-03-21 19:34:10 +0100

    Create Application Startup Listener and move code from Main Clas there, create lighter version of main data file and log terrorism targets

--- src/main/java/com/NowakArtur97/GlobalTerrorismAPI/GlobalTerrorismApiApplication.java
+++ src/main/java/com/NowakArtur97/GlobalTerrorismAPI/GlobalTerrorismApiApplication.java
@@ -1,68 +1,12 @@
 package com.NowakArtur97.GlobalTerrorismAPI;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.util.Iterator;
-
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.monitorjbl.xlsx.StreamingReader;
-
 @SpringBootApplication
-public class GlobalTerrorismApiApplication implements CommandLineRunner {
-
-	private final static String pathToFile = "C:\\Users\\Samsung\\Spring\\eclipse-workspace\\Projects\\GlobalTerrorismAPI\\src\\main\\resources\\data\\globalterrorismdb_0919dist.xlsx";
+public class GlobalTerrorismApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GlobalTerrorismApiApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-
-		File globalTerrorismFile = new File(pathToFile);
-
-		InputStream inputStream = new FileInputStream(globalTerrorismFile);
-
-		Workbook workbook = StreamingReader.builder().rowCacheSize(100).bufferSize(4096).open(inputStream);
-
-		Sheet sheet = workbook.getSheetAt(0);
-
-		Iterator<Row> rowIterator = sheet.rowIterator();
-
-		while (rowIterator.hasNext()) {
-
-			Row row = rowIterator.next();
-
-			Iterator<Cell> cellIterator = row.cellIterator();
-
-			while (cellIterator.hasNext()) {
-
-				Cell cell = cellIterator.next();
-
-				switch (cell.getCellType()) {
-				case STRING:
-					System.out.print(cell.getStringCellValue() + "\t");
-					break;
-				case NUMERIC:
-					System.out.print(cell.getNumericCellValue() + "\t");
-					break;
-				case BOOLEAN:
-					System.out.print(cell.getBooleanCellValue() + "\t");
-					break;
-				default:
-					System.out.print(cell.getStringCellValue());
-				}
-			}
-
-			System.out.println();
-		}
 	}
 }
---
+++ src/main/java/com/NowakArtur97/GlobalTerrorismAPI/eventListener/ApplicationStartupEventListener.java
@@ -0,0 +1,70 @@
+package com.NowakArtur97.GlobalTerrorismAPI.eventListener;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Iterator;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+import com.monitorjbl.xlsx.StreamingReader;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@Slf4j
+public class ApplicationStartupEventListener {
+
+	private final static String pathToFile = "C:\\Users\\Samsung\\Spring\\eclipse-workspace\\Projects\\GlobalTerrorismAPI\\src\\main\\resources\\data\\globalterrorismdb_0919dist-mini.xlsx";
+
+	@EventListener
+	public void onApplicationStartup(ContextRefreshedEvent event) {
+
+		log.info("START");
+		File globalTerrorismFile = new File(pathToFile);
+
+		InputStream inputStream;
+
+		try {
+			
+			inputStream = new FileInputStream(globalTerrorismFile);
+
+			Workbook workbook = StreamingReader.builder().rowCacheSize(10).bufferSize(4096).open(inputStream);
+
+			Sheet sheet = workbook.getSheetAt(0);
+
+			Iterator<Row> rowIterator = sheet.rowIterator();
+
+			while (rowIterator.hasNext()) {
+
+				int targetIndex = 0;
+
+				Row row = rowIterator.next();
+
+				Iterator<Cell> cellIterator = row.cellIterator();
+
+				while (cellIterator.hasNext()) {
+
+					Cell cell = cellIterator.next();
+
+					if (targetIndex == 39 && !cell.getStringCellValue().isBlank()) {
+
+						log.info(cell.getStringCellValue());
+					}
+					
+					targetIndex++;
+				}
+			}
+		} catch (FileNotFoundException e) {
+
+		}
+
+	}
+}
