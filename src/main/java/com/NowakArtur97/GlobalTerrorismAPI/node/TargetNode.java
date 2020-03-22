7c7fadfd75659d2108d316aa8d68f66ef4516dc1
NowakArtur97 <nowart97@gmail.com>
2020-03-22 12:14:19 +0100

    Create Target Node in new node package

---
+++ src/main/java/com/NowakArtur97/GlobalTerrorismAPI/node/TargetNode.java
@@ -0,0 +1,24 @@
+package com.NowakArtur97.GlobalTerrorismAPI.node;
+
+import org.neo4j.ogm.annotation.GeneratedValue;
+import org.neo4j.ogm.annotation.Id;
+import org.neo4j.ogm.annotation.NodeEntity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NodeEntity(label = "Target")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TargetNode {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+	
+	private String target;
+}
