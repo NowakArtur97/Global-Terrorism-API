91fb4d51fe4a7cc0e58451714df28dda84f7ba48
NowakArtur97 <nowart97@gmail.com>
2020-03-25 18:48:26 +0100

    Create Target Model Assembler Test with node to model method test

---
+++ src/test/java/com/NowakArtur97/GlobalTerrorismAPI/assembler/TargetModelAssemblerTest.java
@@ -0,0 +1,50 @@
+package com.NowakArtur97.GlobalTerrorismAPI.assembler;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import com.NowakArtur97.GlobalTerrorismAPI.model.TargetModel;
+import com.NowakArtur97.GlobalTerrorismAPI.node.TargetNode;
+
+@DisplayName("Target Model Assembler Tests")
+@Tag("TargetModelAssembler_Tests")
+public class TargetModelAssemblerTest {
+
+	private TargetModelAssembler targetModelAssembler;
+
+	@BeforeEach
+	public void setUp() {
+
+		targetModelAssembler = new TargetModelAssembler();
+	}
+
+	@Test
+	@DisplayName("when map target node to model")
+	public void when_map_target_node_to_model() {
+
+		Long targetId = 1L;
+		String targetName = "target1";
+		TargetNode targetNode = new TargetNode(targetId, targetName);
+
+		TargetModel targetModel = targetModelAssembler.toModel(targetNode);
+
+		assertAll(
+				() -> assertEquals(targetNode.getId(), targetModel.getId(),
+						() -> "should return model with id: " + targetNode.getId() + ", but was: "
+								+ targetModel.getId()),
+				() -> assertEquals(targetNode.getTarget(), targetModel.getTarget(),
+						() -> "should return model with target: " + targetNode.getTarget() + ", but was: "
+								+ targetModel.getTarget()),
+				() -> assertNotNull(targetModel.getLinks(),
+						() -> "should return model with links, but was: " + targetModel),
+				() -> assertFalse(targetModel.getLinks().isEmpty(),
+						() -> "should return model with links, but was: " + targetModel));
+	}
+}
