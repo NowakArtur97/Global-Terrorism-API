1da55ed3f0b8a03063ebcf79d34cf8724325caab
NowakArtur97 <nowart97@gmail.com>
2020-04-04 10:44:36 +0200

    Create Rest Response Global Entity Exception Handler extending Response Entity Exception Handler for Method Argument Not Valid Exception

---
+++ src/main/java/com/NowakArtur97/GlobalTerrorismAPI/advice/RestResponseGlobalEntityExceptionHandler.java
@@ -0,0 +1,35 @@
+package com.NowakArtur97.GlobalTerrorismAPI.advice;
+
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class RestResponseGlobalEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+		Map<String, Object> body = new LinkedHashMap<>();
+		body.put("timestamp", new Date());
+		body.put("status", status.value());
+
+		List<String> fieldErrors = ex.getBindingResult().getFieldErrors().stream()
+				.map(error -> error.getDefaultMessage()).collect(Collectors.toList());
+
+		body.put("errors", fieldErrors);
+
+		return new ResponseEntity<>(body, headers, status);
+	}
+}
