4e636301089859e018f3b445569fc903eaf0d51d
NowakArtur97 <nowart97@gmail.com>
2020-04-11 17:54:10 +0200

    Perform validation in Patch Helper using Validator

--- src/main/java/com/NowakArtur97/GlobalTerrorismAPI/util/PatchHelper.java
+++ src/main/java/com/NowakArtur97/GlobalTerrorismAPI/util/PatchHelper.java
@@ -1,9 +1,14 @@
 package com.NowakArtur97.GlobalTerrorismAPI.util;
 
+import java.util.Set;
+
 import javax.json.JsonMergePatch;
 import javax.json.JsonPatch;
 import javax.json.JsonStructure;
 import javax.json.JsonValue;
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import javax.validation.Validator;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -18,11 +23,22 @@ public class PatchHelper {
 
 	private final ObjectMapper objectMapper;
 
+	private final Validator valdiator;
+
 	public <T> T patch(JsonPatch jsonPatch, T targetBean, Class<T> beanClass) {
 
 		JsonStructure target = objectMapper.convertValue(targetBean, JsonStructure.class);
 
 		JsonValue patched = jsonPatch.apply(target);
+
+		T beanPatched = objectMapper.convertValue(patched, beanClass);
+
+		Set<ConstraintViolation<T>> violations = valdiator.validate(beanPatched);
+
+		if (!violations.isEmpty()) {
+
+			throw new ConstraintViolationException(violations);
+		}
 
 		return objectMapper.convertValue(patched, beanClass);
 	}
@@ -32,6 +48,15 @@ public class PatchHelper {
 		JsonValue target = objectMapper.convertValue(targetBean, JsonValue.class);
 
 		JsonValue patched = jsonMergePatch.apply(target);
+
+		T beanPatched = objectMapper.convertValue(patched, beanClass);
+
+		Set<ConstraintViolation<T>> violations = valdiator.validate(beanPatched);
+
+		if (!violations.isEmpty()) {
+
+			throw new ConstraintViolationException(violations);
+		}
 
 		return objectMapper.convertValue(patched, beanClass);
 	}
