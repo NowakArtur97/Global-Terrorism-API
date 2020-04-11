6b1910623e18e244361a90219bd8f458e47a71cd
NowakArtur97 <nowart97@gmail.com>
2020-04-11 18:05:09 +0200

    Rename DTO Mapper to DTO Mapper Impl and create DTO Mapper interface

--- src/main/java/com/NowakArtur97/GlobalTerrorismAPI/mapper/DTOMapper.java
+++ src/main/java/com/NowakArtur97/GlobalTerrorismAPI/mapper/DTOMapper.java
@@ -1,21 +1,8 @@
 package com.NowakArtur97.GlobalTerrorismAPI.mapper;
 
-import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import com.NowakArtur97.GlobalTerrorismAPI.dto.DTOEntity;
 
-import lombok.RequiredArgsConstructor;
+public interface DTOMapper {
 
-@Service
-@RequiredArgsConstructor(onConstructor = @__(@Autowired))
-public class DTOMapper {
-
-	private final ModelMapper modelMapper;
-
-	public Object convertToEntity(Object object, DTOEntity dto) {
-
-		return modelMapper.map(object, dto.getClass());
-	}
+	Object convertToEntity(Object object, DTOEntity dto);
 }
---
+++ src/main/java/com/NowakArtur97/GlobalTerrorismAPI/mapper/DTOMapperImpl.java
@@ -0,0 +1,22 @@
+package com.NowakArtur97.GlobalTerrorismAPI.mapper;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.NowakArtur97.GlobalTerrorismAPI.dto.DTOEntity;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class DTOMapperImpl implements DTOMapper {
+
+	private final ModelMapper modelMapper;
+
+	@Override
+	public Object convertToEntity(Object object, DTOEntity dto) {
+
+		return modelMapper.map(object, dto.getClass());
+	}
+}
