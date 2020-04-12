ee47d3ea69b891fb9479d57092617594b416fb2e
NowakArtur97 <nowart97@gmail.com>
2020-04-12 16:57:05 +0200

    Create Name Generation for Nested Test classes in Name With Spaces Generator

--- src/test/java/com/NowakArtur97/GlobalTerrorismAPI/testUtils/nameGenerator/NameWithSpacesGenerator.java
+++ src/test/java/com/NowakArtur97/GlobalTerrorismAPI/testUtils/nameGenerator/NameWithSpacesGenerator.java
@@ -6,6 +6,12 @@ import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
 
 public class NameWithSpacesGenerator extends ReplaceUnderscores {
 
+	private final String wordEndingTestName = "should";
+
+	private final String signReplacedInMethodName = "_";
+
+	private final String signReplacingInMethodName = " ";
+
 	@Override
 	public String generateDisplayNameForClass(Class<?> testClass) {
 
@@ -17,32 +23,43 @@ public class NameWithSpacesGenerator extends ReplaceUnderscores {
 	}
 
 	@Override
-	public String generateDisplayNameForMethod(Class<?> testClass, Method testMethod) {
+	public String generateDisplayNameForNestedClass(Class<?> nestedClass) {
 
-		String testMethodName = testMethod.getName();
+		String nestedClassMethodName = nestedClass.getSimpleName();
 
-		int indexOfShouldWord = testMethodName.indexOf("should");
-
-		String displayName = testMethodName.substring(0, indexOfShouldWord - 1).replace("_", " ");
+		String displayName = addSpacesBetweenWords(nestedClassMethodName);
 
 		return displayName;
 	}
 
-	private String addSpacesBetweenWords(String classMethodName) {
+	@Override
+	public String generateDisplayNameForMethod(Class<?> testClass, Method testMethod) {
 
-		int wordLength = classMethodName.length();
-		
+		String testMethodName = testMethod.getName();
+
+		int indexOfShouldWord = testMethodName.indexOf(wordEndingTestName);
+
+		String displayName = testMethodName.substring(0, indexOfShouldWord - 1).replace(signReplacedInMethodName,
+				signReplacingInMethodName);
+
+		return displayName;
+	}
+
+	private String addSpacesBetweenWords(String className) {
+
+		int wordLength = className.length();
+
 		StringBuilder result = new StringBuilder();
-		result.append(classMethodName.charAt(0));
+		result.append(className.charAt(0));
 
 		for (int i = 1; i < wordLength; i++) {
 
-			if (Character.isUpperCase(classMethodName.charAt(i))) {
+			if (Character.isUpperCase(className.charAt(i))) {
 
 				result.append(' ');
 			}
 
-			result.append(classMethodName.charAt(i));
+			result.append(className.charAt(i));
 		}
 
 		return result.toString();
