18bbd1e89a824c16890e93a9cd3677ebe58e373a
NowakArtur97 <nowart97@gmail.com>
2020-04-12 16:01:18 +0200

    Delete Target Mapper interface and implementation

--- src/main/java/com/NowakArtur97/GlobalTerrorismAPI/mapper/TargetMapper.java
+++
@@ -1,9 +0,0 @@
-package com.NowakArtur97.GlobalTerrorismAPI.mapper;
-
-import com.NowakArtur97.GlobalTerrorismAPI.dto.TargetDTO;
-import com.NowakArtur97.GlobalTerrorismAPI.node.TargetNode;
-
-public interface TargetMapper {
-
-	TargetNode mapDTOToNode(TargetDTO targetDTO);
-}
