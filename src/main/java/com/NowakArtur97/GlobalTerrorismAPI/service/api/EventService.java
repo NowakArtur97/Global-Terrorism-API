3ee42fe2f360bbb5903f39f66390b62346b2689b
NowakArtur97 <nowart97@gmail.com>
2020-04-20 12:25:41 +0200

    Create Find All method in Target Service Interface and Implementation

--- src/main/java/com/NowakArtur97/GlobalTerrorismAPI/service/api/EventService.java
+++ src/main/java/com/NowakArtur97/GlobalTerrorismAPI/service/api/EventService.java
@@ -1,8 +1,13 @@
 package com.NowakArtur97.GlobalTerrorismAPI.service.api;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import com.NowakArtur97.GlobalTerrorismAPI.node.EventNode;
 
 public interface EventService {
+
+	Page<EventNode> findAll(Pageable pageable);
 
 	EventNode save(EventNode eventNode);
 }
--- src/main/java/com/NowakArtur97/GlobalTerrorismAPI/service/impl/EventServiceImpl.java
+++ src/main/java/com/NowakArtur97/GlobalTerrorismAPI/service/impl/EventServiceImpl.java
@@ -1,7 +1,10 @@
 package com.NowakArtur97.GlobalTerrorismAPI.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.NowakArtur97.GlobalTerrorismAPI.node.EventNode;
 import com.NowakArtur97.GlobalTerrorismAPI.repository.EventRepository;
@@ -16,8 +19,16 @@ public class EventServiceImpl implements EventService {
 	private final EventRepository eventRepository;
 
 	@Override
+	@Transactional(readOnly = true)
+	public Page<EventNode> findAll(Pageable pageable) {
+
+		return eventRepository.findAll(pageable);
+	}
+
+	@Override
 	public EventNode save(EventNode eventNode) {
 
 		return eventRepository.save(eventNode);
 	}
+
 }
