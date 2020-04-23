ff02afdc177782e7e0510cd9d71973364c54e1a5
NowakArtur97 <nowart97@gmail.com>
2020-04-23 10:08:10 +0200

    Create Post Request Mapping in Event Controller for saving new Events

--- src/main/java/com/NowakArtur97/GlobalTerrorismAPI/controller/EventController.java
+++ src/main/java/com/NowakArtur97/GlobalTerrorismAPI/controller/EventController.java
@@ -2,6 +2,8 @@ package com.NowakArtur97.GlobalTerrorismAPI.controller;
 
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -13,12 +15,17 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.NowakArtur97.GlobalTerrorismAPI.annotation.ApiPageable;
 import com.NowakArtur97.GlobalTerrorismAPI.assembler.EventModelAssembler;
+import com.NowakArtur97.GlobalTerrorismAPI.dto.EventDTO;
 import com.NowakArtur97.GlobalTerrorismAPI.exception.EventNotFoundException;
+import com.NowakArtur97.GlobalTerrorismAPI.exception.TargetNotFoundException;
 import com.NowakArtur97.GlobalTerrorismAPI.model.ErrorResponse;
 import com.NowakArtur97.GlobalTerrorismAPI.model.EventModel;
 import com.NowakArtur97.GlobalTerrorismAPI.node.EventNode;
@@ -72,7 +79,21 @@ public class EventController {
 		return eventService.findById(id).map(eventModelAssembler::toModel).map(ResponseEntity::ok)
 				.orElseThrow(() -> new EventNotFoundException(id));
 	}
-	
+
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED) // Added to remove the default 200 status added by Swagger
+	@ApiOperation(value = "Add Event", notes = "Add new Event")
+	@ApiResponses({ @ApiResponse(code = 201, message = "Successfully added new Event", response = EventModel.class),
+			@ApiResponse(code = 400, message = "Incorrectly entered data", response = ErrorResponse.class) })
+	public ResponseEntity<EventModel> addEvent(
+			@ApiParam(value = "New Event", name = "event", required = true) @RequestBody @Valid EventDTO eventDTO) {
+
+		EventNode eventNode = eventService.saveOrUpdate(null, eventDTO);
+
+		return new ResponseEntity<>((Optional.of(eventNode)).map(eventModelAssembler::toModel)
+				.orElseThrow(() -> new TargetNotFoundException(eventNode.getId())), HttpStatus.CREATED);
+	}
+
 	@DeleteMapping(path = "/{id}")
 	@ApiOperation(value = "Delete Event by id", notes = "Provide an id to delete specific Event")
 	@ApiResponses({ @ApiResponse(code = 200, message = "Successfully deleted Event", response = EventModel.class),
