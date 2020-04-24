baa115595178aa8337446c332099a8a2ee8b7e76
NowakArtur97 <nowart97@gmail.com>
2020-04-24 12:38:50 +0200

    Disable Event Controller Test on Linux

--- src/test/java/com/NowakArtur97/GlobalTerrorismAPI/controller/EventControllerTest.java
+++ src/test/java/com/NowakArtur97/GlobalTerrorismAPI/controller/EventControllerTest.java
@@ -32,6 +32,8 @@ import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledOnOs;
+import org.junit.jupiter.api.condition.OS;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
@@ -68,6 +70,7 @@ import com.ibm.icu.util.Calendar;
 @ExtendWith(MockitoExtension.class)
 @DisplayNameGeneration(NameWithSpacesGenerator.class)
 @Tag("EventController_Tests")
+@DisabledOnOs(OS.LINUX)
 class EventControllerTest {
 
 	private final String EVENT_BASE_PATH = "http://localhost:8080/api/events";
