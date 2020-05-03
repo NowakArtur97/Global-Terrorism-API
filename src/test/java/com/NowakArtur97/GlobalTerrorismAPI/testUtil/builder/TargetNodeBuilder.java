9d7f657c4659f63e4ae728bdd0c6c6cc94106eb0
NowakArtur97 <nowart97@gmail.com>
2020-05-03 10:25:51 +0200

    Create Taget Node Builder with default values for testing

---
+++ src/test/java/com/NowakArtur97/GlobalTerrorismAPI/testUtil/builder/TargetNodeBuilder.java
@@ -0,0 +1,29 @@
+package com.NowakArtur97.GlobalTerrorismAPI.testUtil.builder;
+
+import com.NowakArtur97.GlobalTerrorismAPI.node.TargetNode;
+
+final public class TargetNodeBuilder {
+
+	private Long id = 1L;
+
+	private String target = "target";
+
+	public TargetNodeBuilder withId(Long id) {
+
+		this.id = id;
+
+		return this;
+	}
+
+	public TargetNodeBuilder withTarget(String target) {
+
+		this.target = target;
+
+		return this;
+	}
+
+	public TargetNode build() {
+
+		return new TargetNode(id, target);
+	}
+}
