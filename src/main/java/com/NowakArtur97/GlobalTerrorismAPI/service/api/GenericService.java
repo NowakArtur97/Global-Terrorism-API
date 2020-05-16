e87684aca8f0639c59017fcf869094950698f95d
NowakArtur97 <nowart97@gmail.com>
2020-05-16 12:01:00 +0200

    Create Generic Service interface

---
+++ src/main/java/com/NowakArtur97/GlobalTerrorismAPI/service/api/GenericService.java
@@ -0,0 +1,22 @@
+package com.NowakArtur97.GlobalTerrorismAPI.service.api;
+
+import com.NowakArtur97.GlobalTerrorismAPI.dto.DTONode;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.Optional;
+
+public interface GenericService<T> {
+
+    Optional<T> findById(Long id);
+
+    Page<T> findAll(Pageable pageable);
+
+    T save(T node);
+
+    T saveNew(DTONode dto);
+
+    T update(T node, DTONode dto);
+
+    Optional<T> delete(Long id);
+}
