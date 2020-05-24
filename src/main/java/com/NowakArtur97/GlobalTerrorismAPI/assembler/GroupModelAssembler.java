956ad70c8d1e8b514f1624b6a0793af036dfa6ac
NowakArtur97 <nowart97@gmail.com>
2020-05-24 12:35:02 +0200

    Create Group Model Assembler

---
+++ src/main/java/com/NowakArtur97/GlobalTerrorismAPI/assembler/GroupModelAssembler.java
@@ -0,0 +1,40 @@
+package com.NowakArtur97.GlobalTerrorismAPI.assembler;
+
+import com.NowakArtur97.GlobalTerrorismAPI.mapper.ObjectMapper;
+import com.NowakArtur97.GlobalTerrorismAPI.model.GroupModel;
+import com.NowakArtur97.GlobalTerrorismAPI.node.GroupNode;
+import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
+import org.springframework.stereotype.Component;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@Component
+public class GroupModelAssembler extends RepresentationModelAssemblerSupport<GroupNode, GroupModel> {
+
+    private final EventModelAssembler eventModelAssembler;
+
+    private final ObjectMapper objectMapper;
+
+    public GroupModelAssembler(EventModelAssembler eventModelAssembler, ObjectMapper objectMapper) {
+
+        super(GroupController.class, GroupModel.class);
+
+        this.eventModelAssembler = eventModelAssembler;
+
+        this.objectMapper = objectMapper;
+    }
+
+    @Override
+    public GroupModel toModel(GroupNode groupNode) {
+
+        GroupModel groupModel = objectMapper.map(groupNode, GroupModel.class);
+
+//        eventModel
+//                .setTarget(eventNode.getTarget() != null ? targetModelAssembler.toModel(eventNode.getTarget()) : null);
+
+        groupModel.add(linkTo(methodOn(GroupController.class).findById(groupModel.getId())).withSelfRel());
+
+        return groupModel;
+    }
+}
