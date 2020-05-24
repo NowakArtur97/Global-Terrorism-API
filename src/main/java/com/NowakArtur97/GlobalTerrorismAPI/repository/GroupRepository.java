2596bc514c6be8ffed363933d04ff77c7f2b172d
NowakArtur97 <nowart97@gmail.com>
2020-05-24 12:25:16 +0200

    Make Group repository extend Base Repository instead of Neo4jRepository

--- src/main/java/com/NowakArtur97/GlobalTerrorismAPI/repository/GroupRepository.java
+++ src/main/java/com/NowakArtur97/GlobalTerrorismAPI/repository/GroupRepository.java
@@ -1,8 +1,7 @@
 package com.NowakArtur97.GlobalTerrorismAPI.repository;
 
 import com.NowakArtur97.GlobalTerrorismAPI.node.GroupNode;
-import org.springframework.data.neo4j.repository.Neo4jRepository;
 
-public interface GroupRepository extends Neo4jRepository<GroupNode, Long> {
+public interface GroupRepository extends BaseRepository<GroupNode> {
 
 }
