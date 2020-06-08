e71b917fc34f8202faba502b7add4c9faf1b63d6
NowakArtur97 <nowart97@gmail.com>
2020-06-08 12:57:05 +0200

    Add List of Events validation and validation messages in Group DTO

--- src/main/java/com/NowakArtur97/GlobalTerrorismAPI/dto/GroupDTO.java
+++ src/main/java/com/NowakArtur97/GlobalTerrorismAPI/dto/GroupDTO.java
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.NotEmpty;
 import java.util.List;
 
 @ApiModel(description = "Model responsible for Group validation")
@@ -24,6 +24,6 @@ public class GroupDTO implements DTONode, Group {
 
     @ApiModelProperty(notes = "The event's caused by the group")
     @Valid
-    @NotNull(message = "{group.eventsCaused.notNull}")
+    @NotEmpty(message = "{group.eventsCaused.notEmpty}")
     private List<EventDTO> eventsCaused;
 }
