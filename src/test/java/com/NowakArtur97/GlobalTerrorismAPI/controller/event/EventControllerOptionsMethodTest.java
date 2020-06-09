78e6bacb29dda2b2ac15718a4ca66918d0c9d556
NowakArtur97 <nowart97@gmail.com>
2020-06-09 12:26:24 +0200

    Create tests for Event Options Request Method

---
+++ src/test/java/com/NowakArtur97/GlobalTerrorismAPI/controller/event/EventControllerOptionsMethodTest.java
@@ -0,0 +1,106 @@
+package com.NowakArtur97.GlobalTerrorismAPI.controller.event;
+
+import com.NowakArtur97.GlobalTerrorismAPI.assembler.EventModelAssembler;
+import com.NowakArtur97.GlobalTerrorismAPI.controller.EventController;
+import com.NowakArtur97.GlobalTerrorismAPI.controller.GenericRestController;
+import com.NowakArtur97.GlobalTerrorismAPI.dto.EventDTO;
+import com.NowakArtur97.GlobalTerrorismAPI.model.EventModel;
+import com.NowakArtur97.GlobalTerrorismAPI.node.EventNode;
+import com.NowakArtur97.GlobalTerrorismAPI.service.api.GenericService;
+import com.NowakArtur97.GlobalTerrorismAPI.testUtil.nameGenerator.NameWithSpacesGenerator;
+import com.NowakArtur97.GlobalTerrorismAPI.util.PatchHelper;
+import com.NowakArtur97.GlobalTerrorismAPI.util.ViolationHelper;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verifyNoInteractions;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.options;
+
+@ExtendWith(MockitoExtension.class)
+@DisplayNameGeneration(NameWithSpacesGenerator.class)
+@Tag("EventController_Tests")
+public class EventControllerOptionsMethodTest {
+
+    private final String EVENT_BASE_PATH = "http://localhost:8080/api/events";
+
+    private MockMvc mockMvc;
+
+    private GenericRestController<EventModel, EventDTO> eventController;
+
+    @Mock
+    private GenericService<EventNode, EventDTO> eventService;
+
+    @Mock
+    private EventModelAssembler modelAssembler;
+
+    @Mock
+    private PagedResourcesAssembler<EventNode> pagedResourcesAssembler;
+
+    @Mock
+    private PatchHelper patchHelper;
+
+    @Mock
+    private ViolationHelper<EventNode, EventDTO> violationHelper;
+
+    @BeforeEach
+    private void setUp() {
+
+        eventController = new EventController(eventService, modelAssembler, pagedResourcesAssembler, patchHelper,
+                violationHelper);
+
+        mockMvc = MockMvcBuilders.standaloneSetup(eventController).build();
+    }
+
+    @Test
+    @SneakyThrows
+    void when_show_endpoint_options_for_collection_should_show_possible_request_methods() {
+
+        MvcResult mvcResult = mockMvc.perform(options(EVENT_BASE_PATH)).andReturn();
+        String allowedMethods = mvcResult.getResponse().getHeader("allow");
+
+        System.out.println(allowedMethods);
+
+        assertAll(
+                () -> assertNotNull(allowedMethods, () -> "should header contain allowed methods, but wasn't"),
+                () -> assertTrue(allowedMethods.contains("GET"), () -> "should contain GET option, but was: " + allowedMethods),
+                () -> assertTrue(allowedMethods.contains("POST"), () -> "should contain POST option, but was: " + allowedMethods),
+                () -> assertTrue(allowedMethods.contains("OPTIONS"), () -> "should contain OPTIONS option, but was: " + allowedMethods),
+                () -> verifyNoInteractions(eventService), () -> verifyNoInteractions(pagedResourcesAssembler),
+                () -> verifyNoInteractions(modelAssembler), () -> verifyNoInteractions(patchHelper),
+                () -> verifyNoInteractions(violationHelper));
+    }
+
+    @Test
+    @SneakyThrows
+    void when_show_endpoint_options_for_singular_resource_should_show_possible_request_methods() {
+
+        Long eventId = 1L;
+
+        String linkWithParameter = EVENT_BASE_PATH + "/" + "{id}";
+
+        MvcResult mvcResult = mockMvc.perform(options(linkWithParameter, eventId)).andReturn();
+        String allowedMethods = mvcResult.getResponse().getHeader("allow");
+
+        assertAll(
+                () -> assertNotNull(allowedMethods, () -> "should header contain allowed methods, but wasn't"),
+                () -> assertTrue(allowedMethods.contains("GET"), () -> "should contain GET option, but was: " + allowedMethods),
+                () -> assertTrue(allowedMethods.contains("PUT"), () -> "should contain PUT option, but was: " + allowedMethods),
+                () -> assertTrue(allowedMethods.contains("PATCH"), () -> "should contain PATCH option, but was: " + allowedMethods),
+                () -> assertTrue(allowedMethods.contains("DELETE"), () -> "should contain DELETE option, but was: " + allowedMethods),
+                () -> assertTrue(allowedMethods.contains("OPTIONS"), () -> "should contain OPTIONS option, but was: " + allowedMethods),
+                () -> verifyNoInteractions(eventService), () -> verifyNoInteractions(pagedResourcesAssembler),
+                () -> verifyNoInteractions(modelAssembler), () -> verifyNoInteractions(patchHelper),
+                () -> verifyNoInteractions(violationHelper));
+    }
+}
