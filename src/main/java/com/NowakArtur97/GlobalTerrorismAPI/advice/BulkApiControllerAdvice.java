ee73254039dc9e477c06f4af26076d247469db24
NowakArtur97 <nowart97@gmail.com>
2020-06-26 10:51:28 +0200

    Create Bulk Api Controller Advice for handling Http Client Error Exception

--- src/main/java/com/NowakArtur97/GlobalTerrorismAPI/advice/BulkApiControllerAdvice.java
+++ src/main/java/com/NowakArtur97/GlobalTerrorismAPI/advice/BulkApiControllerAdvice.java
@@ -1,20 +1,20 @@
 package com.NowakArtur97.GlobalTerrorismAPI.advice;
 
-import com.NowakArtur97.GlobalTerrorismAPI.controller.GenericRestControllerImpl;
-import com.NowakArtur97.GlobalTerrorismAPI.exception.ResourceNotFoundException;
 import com.NowakArtur97.GlobalTerrorismAPI.model.ErrorResponse;
+import com.github.wnameless.spring.bulkapi.BulkApiController;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.client.HttpClientErrorException;
 
 import java.time.LocalDateTime;
 
-@RestControllerAdvice(basePackageClasses = GenericRestControllerImpl.class)
-public class GenericRestControllerAdvice {
+@RestControllerAdvice(basePackageClasses = BulkApiController.class)
+public class BulkApiControllerAdvice {
 
-    @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<ErrorResponse> handleResourceNotFoundException(ResourceNotFoundException exception) {
+    @ExceptionHandler(HttpClientErrorException.class)
+    public ResponseEntity<ErrorResponse> handleHttpClientErrorException(HttpClientErrorException exception) {
 
         ErrorResponse errorResponse = new ErrorResponse(LocalDateTime.now(), HttpStatus.NOT_FOUND.value());
 
--- src/main/java/com/NowakArtur97/GlobalTerrorismAPI/advice/RestResponseGlobalEntityExceptionHandler.java
+++ src/main/java/com/NowakArtur97/GlobalTerrorismAPI/advice/RestResponseGlobalEntityExceptionHandler.java
@@ -1,22 +1,20 @@
 package com.NowakArtur97.GlobalTerrorismAPI.advice;
 
-import java.time.LocalDateTime;
-
-import javax.validation.ConstraintViolationException;
-
+import com.NowakArtur97.GlobalTerrorismAPI.model.ErrorResponse;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import com.NowakArtur97.GlobalTerrorismAPI.model.ErrorResponse;
+import javax.validation.ConstraintViolationException;
+import java.time.LocalDateTime;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class RestResponseGlobalEntityExceptionHandler extends ResponseEntityExceptionHandler {
 
 	@Override
@@ -25,7 +23,7 @@ public class RestResponseGlobalEntityExceptionHandler extends ResponseEntityExce
 
 		ErrorResponse errorResponse = new ErrorResponse(LocalDateTime.now(), HttpStatus.BAD_REQUEST.value());
 
-		ex.getBindingResult().getFieldErrors().stream()
+		ex.getBindingResult().getFieldErrors()
 				.forEach(error -> errorResponse.addError(error.getDefaultMessage()));
 
 		return new ResponseEntity<>(errorResponse, headers, status);
@@ -49,7 +47,7 @@ public class RestResponseGlobalEntityExceptionHandler extends ResponseEntityExce
 
 		ErrorResponse errorResponse = new ErrorResponse(LocalDateTime.now(), HttpStatus.BAD_REQUEST.value());
 
-		ex.getConstraintViolations().stream().forEach(error -> errorResponse.addError(error.getMessage()));
+		ex.getConstraintViolations().forEach(error -> errorResponse.addError(error.getMessage()));
 
 		return new ResponseEntity<>(errorResponse, new HttpHeaders(), HttpStatus.BAD_REQUEST);
 	}
