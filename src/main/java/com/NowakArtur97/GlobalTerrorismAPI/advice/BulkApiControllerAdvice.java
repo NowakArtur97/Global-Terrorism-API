bd53cb0ef79170a9666fe4b29e9e7a9cfc8ade27
NowakArtur97 <nowart97@gmail.com>
2020-06-27 09:03:47 +0200

    Refactor Bulk Api Controller Advice

--- src/main/java/com/NowakArtur97/GlobalTerrorismAPI/advice/BulkApiControllerAdvice.java
+++ src/main/java/com/NowakArtur97/GlobalTerrorismAPI/advice/BulkApiControllerAdvice.java
@@ -1,35 +1,17 @@
 package com.NowakArtur97.GlobalTerrorismAPI.advice;
 
-import com.NowakArtur97.GlobalTerrorismAPI.model.ErrorResponse;
 import com.github.wnameless.spring.bulkapi.BulkApiController;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.client.HttpClientErrorException;
 
-import java.time.LocalDateTime;
-import java.util.Arrays;
-
 @RestControllerAdvice(basePackageClasses = BulkApiController.class)
 public class BulkApiControllerAdvice {
 
     @ExceptionHandler(HttpClientErrorException.class)
-    public ResponseEntity<ErrorResponse> handleHttpClientErrorException(HttpClientErrorException exception) {
+    public ResponseEntity<Object> handleHttpClientErrorException(HttpClientErrorException exception) {
 
-        String message = exception.getResponseBodyAsString().replace("\"", "");
-
-        ErrorResponse errorResponse = new ErrorResponse(LocalDateTime.now(), exception.getStatusCode().value());
-
-        Arrays.stream(getMessagesFromBody(message)).forEach(errorResponse::addError);
-
-        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
-    }
-
-    private String[] getMessagesFromBody(String message) {
-
-        return message
-                .substring(message.indexOf("[") + 1, message.indexOf("]"))
-                .split(",");
+        return new ResponseEntity<>(exception.getResponseBodyAsString(), exception.getResponseHeaders(), exception.getStatusCode());
     }
 }
