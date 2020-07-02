bba69f69fc8d754b1feba8e708e812a8589e4cb3
NowakArtur97 <nowart97@gmail.com>
2020-07-02 15:50:42 +0200

    Implement Jwt Util Service and add methods for validation and creation of Jwt

--- src/main/java/com/NowakArtur97/GlobalTerrorismAPI/util/jw/JwtUtil.java
+++ src/main/java/com/NowakArtur97/GlobalTerrorismAPI/util/jw/JwtUtil.java
@@ -1,11 +1,16 @@
 package com.NowakArtur97.GlobalTerrorismAPI.util.jw;
 
 import io.jsonwebtoken.Claims;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Date;
 import java.util.function.Function;
 
 public interface JwtUtil {
+
+    String generateToken(UserDetails userDetails);
+
+    Boolean validateToken(String token, UserDetails userDetail);
 
     String extractUsername(String token);
 
