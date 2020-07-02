26eb6883981bb6a8b6e63551e8c6b31d8cb6dd70
NowakArtur97 <nowart97@gmail.com>
2020-07-02 14:14:15 +0200

    Add default Role to User when registrating valid user

--- src/main/java/com/NowakArtur97/GlobalTerrorismAPI/service/impl/UserServiceImpl.java
+++ src/main/java/com/NowakArtur97/GlobalTerrorismAPI/service/impl/UserServiceImpl.java
@@ -2,15 +2,20 @@ package com.NowakArtur97.GlobalTerrorismAPI.service.impl;
 
 import com.NowakArtur97.GlobalTerrorismAPI.dto.UserDTO;
 import com.NowakArtur97.GlobalTerrorismAPI.mapper.ObjectMapper;
+import com.NowakArtur97.GlobalTerrorismAPI.node.RoleNode;
 import com.NowakArtur97.GlobalTerrorismAPI.node.UserNode;
 import com.NowakArtur97.GlobalTerrorismAPI.repository.UserRepository;
 import com.NowakArtur97.GlobalTerrorismAPI.service.api.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Set;
+
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
+
+    private final static RoleNode DEFAULT_USER_ROLE = new RoleNode("user");
 
     private final UserRepository userRepository;
 
@@ -20,6 +25,8 @@ public class UserServiceImpl implements UserService {
     public UserNode register(UserDTO userDTO) {
 
         UserNode userNode = objectMapper.map(userDTO, UserNode.class);
+
+        userNode.setRoles(Set.of(DEFAULT_USER_ROLE));
 
         return userRepository.save(userNode);
     }
