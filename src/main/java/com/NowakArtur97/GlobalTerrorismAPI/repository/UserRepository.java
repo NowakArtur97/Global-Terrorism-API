05f73d1ab3b834761ef2dcf0c18d364c74298c3e
NowakArtur97 <nowart97@gmail.com>
2020-07-02 13:42:50 +0200

    Create User Repository

---
+++ src/main/java/com/NowakArtur97/GlobalTerrorismAPI/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.NowakArtur97.GlobalTerrorismAPI.repository;
+
+import com.NowakArtur97.GlobalTerrorismAPI.node.UserNode;
+
+public interface UserRepository extends BaseRepository<UserNode> {
+
+}
