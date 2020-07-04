16e3da3ae102f6c2c7a0b457e2ae1d4ba5980c43
NowakArtur97 <nowart97@gmail.com>
2020-07-04 07:36:19 +0200

    Create Unique User Name Constraint Validator

---
+++ src/main/java/com/NowakArtur97/GlobalTerrorismAPI/validation/UniqueUsernameConstraintValidator.java
@@ -0,0 +1,26 @@
+package com.NowakArtur97.GlobalTerrorismAPI.validation;
+
+import com.NowakArtur97.GlobalTerrorismAPI.annotation.UniqueUserName;
+import com.NowakArtur97.GlobalTerrorismAPI.service.api.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+@Component
+@RequiredArgsConstructor
+public class UniqueUsernameConstraintValidator implements ConstraintValidator<UniqueUserName, String> {
+
+    private final UserService userService;
+
+    @Override
+    public void initialize(UniqueUserName constraintAnnotation) {
+    }
+
+    @Override
+    public boolean isValid(String userName, ConstraintValidatorContext context) {
+
+        return userService.findByUserName(userName).isEmpty();
+    }
+}
