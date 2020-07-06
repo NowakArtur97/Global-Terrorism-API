8e1c9a6430deace348064be97c9e3582afd9a4c8
NowakArtur97 <nowart97@gmail.com>
2020-07-06 07:36:11 +0200

    Create new password rule in Password Constraint Validator where password cannot be on popular passwords list

--- src/main/java/com/NowakArtur97/GlobalTerrorismAPI/validator/PasswordConstraintValidator.java
+++ src/main/java/com/NowakArtur97/GlobalTerrorismAPI/validator/PasswordConstraintValidator.java
@@ -2,17 +2,39 @@ package com.NowakArtur97.GlobalTerrorismAPI.validator;
 
 import com.NowakArtur97.GlobalTerrorismAPI.annotation.validation.ValidPassword;
 import org.passay.*;
+import org.passay.dictionary.WordListDictionary;
+import org.passay.dictionary.WordLists;
+import org.passay.dictionary.sort.ArraysSort;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class PasswordConstraintValidator implements ConstraintValidator<ValidPassword, String> {
 
+    private DictionaryRule notCommonPasswordRule;
+
     @Override
     public void initialize(ValidPassword constraintAnnotation) {
+
+        try {
+            String invalidPasswordList = this.getClass().getResource("/validation/common-passwords-list.txt").getFile();
+
+            notCommonPasswordRule = new DictionaryRule(
+                    new WordListDictionary(WordLists.createFromReader(
+                            new FileReader[]{
+                                    new FileReader(invalidPasswordList)
+                            },
+                            false,
+                            new ArraysSort()
+                    )));
+        } catch (IOException exception) {
+            throw new RuntimeException("Could not find list of common passwords", exception);
+        }
     }
 
     @Override
@@ -28,7 +50,8 @@ public class PasswordConstraintValidator implements ConstraintValidator<ValidPas
                 new CharacterRule(EnglishCharacterData.LowerCase, 1),
                 new CharacterRule(EnglishCharacterData.Digit, 1),
                 new CharacterRule(EnglishCharacterData.Special, 1),
-                new WhitespaceRule()
+                new WhitespaceRule(),
+                notCommonPasswordRule
         ));
 
         RuleResult result = validator.validate(new PasswordData(password));
