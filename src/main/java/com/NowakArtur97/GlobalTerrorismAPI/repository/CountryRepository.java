9fc5d620559c2eab354bfa8fea85a1ca26c79688
NowakArtur97 <nowart97@gmail.com>
2020-07-16 07:59:04 +0200

    Create Country Repository

---
+++ src/main/java/com/NowakArtur97/GlobalTerrorismAPI/repository/CountryRepository.java
@@ -0,0 +1,7 @@
+package com.NowakArtur97.GlobalTerrorismAPI.repository;
+
+import com.NowakArtur97.GlobalTerrorismAPI.node.CountryNode;
+
+public interface CountryRepository extends BaseRepository<CountryNode> {
+
+}
