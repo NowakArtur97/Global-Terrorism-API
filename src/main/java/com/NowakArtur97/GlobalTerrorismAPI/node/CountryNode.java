fb5fed3eafd128cab6a4ebf61df77449ad9a263e
NowakArtur97 <nowart97@gmail.com>
2020-07-16 07:31:10 +0200

    Create Country Node

---
+++ src/main/java/com/NowakArtur97/GlobalTerrorismAPI/node/CountryNode.java
@@ -0,0 +1,13 @@
+package com.NowakArtur97.GlobalTerrorismAPI.node;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.neo4j.ogm.annotation.NodeEntity;
+
+@NodeEntity(label = "Country")
+@Data
+@NoArgsConstructor
+public class CountryNode extends Node {
+
+    private String name;
+}
