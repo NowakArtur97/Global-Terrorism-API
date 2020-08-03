40fe50f3fa3c20bb8f2e424496f5114523c34a26
NowakArtur97 <nowart97@gmail.com>
2020-08-03 08:07:00 +0200

    Refactor Target Service Impl test

--- src/test/java/com/NowakArtur97/GlobalTerrorismAPI/service/impl/TargetServiceImplTest.java
+++ src/test/java/com/NowakArtur97/GlobalTerrorismAPI/service/impl/TargetServiceImplTest.java
@@ -126,19 +126,27 @@ class TargetServiceImplTest {
 
         Long expectedTargetId = 1L;
 
-        TargetNode targetExpected = (TargetNode) targetBuilder.withId(expectedTargetId).build(ObjectType.NODE);
+        CountryNode countryNodeExpected = (CountryNode) countryBuilder.build(ObjectType.NODE);
+        TargetNode targetNodeExpected = (TargetNode) targetBuilder.withId(expectedTargetId)
+                .withCountry(countryNodeExpected).build(ObjectType.NODE);
 
-        when(targetRepository.findById(expectedTargetId)).thenReturn(Optional.of(targetExpected));
+        when(targetRepository.findById(expectedTargetId)).thenReturn(Optional.of(targetNodeExpected));
 
         Optional<TargetNode> targetActualOptional = targetService.findById(expectedTargetId);
 
-        TargetNode targetActual = targetActualOptional.get();
+        TargetNode targetNodeActual = targetActualOptional.get();
 
-        assertAll(  () -> assertEquals(targetExpected.getId(), targetActual.getId(),
-                        () -> "should return target with id: " + expectedTargetId + ", but was" + targetActual.getId()),
-                () -> assertEquals(targetExpected.getTarget(), targetActual.getTarget(),
-                        () -> "should return target with target: " + targetExpected.getTarget() + ", but was"
-                                + targetActual.getTarget()),
+        assertAll(() -> assertEquals(targetNodeExpected.getId(), targetNodeActual.getId(),
+                () -> "should return target with id: " + expectedTargetId + ", but was" + targetNodeActual.getId()),
+                () -> assertEquals(targetNodeExpected.getTarget(), targetNodeActual.getTarget(),
+                        () -> "should return target with target: " + targetNodeExpected.getTarget() + ", but was"
+                                + targetNodeActual.getTarget()),
+                () -> assertEquals(targetNodeExpected.getCountryOfOrigin().getId(), targetNodeActual.getCountryOfOrigin().getId(),
+                        () -> "should return target node with country id: " + targetNodeExpected.getCountryOfOrigin().getId()
+                                + ", but was: " + targetNodeActual.getId()),
+                () -> assertEquals(targetNodeExpected.getCountryOfOrigin().getName(), targetNodeActual.getCountryOfOrigin().getName(),
+                        () -> "should return target node with country name: " + targetNodeExpected.getCountryOfOrigin().getName()
+                                + ", but was: " + targetNodeActual.getCountryOfOrigin()),
                 () -> verify(targetRepository, times(1)).findById(expectedTargetId),
                 () -> verifyNoMoreInteractions(targetRepository),
                 () -> verifyNoInteractions(objectMapper),
@@ -213,15 +221,13 @@ class TargetServiceImplTest {
     @Test
     void when_save_new_target_should_return_new_target() {
 
-        Long targetId = 1L;
-
         CountryDTO countryDTOExpected = (CountryDTO) countryBuilder.build(ObjectType.DTO);
         TargetDTO targetDTOExpected = (TargetDTO) targetBuilder.withCountry(countryDTOExpected).build(ObjectType.DTO);
 
         CountryNode countryNodeExpected = (CountryNode) countryBuilder.build(ObjectType.NODE);
         TargetNode targetNodeExpectedBeforeSetCountry = (TargetNode) targetBuilder.withId(null).withCountry(null).build(ObjectType.NODE);
         TargetNode targetNodeExpectedBeforeSave = (TargetNode) targetBuilder.withId(null).withCountry(countryNodeExpected).build(ObjectType.NODE);
-        TargetNode targetNodeExpected = (TargetNode) targetBuilder.withId(targetId).withCountry(countryNodeExpected).build(ObjectType.NODE);
+        TargetNode targetNodeExpected = (TargetNode) targetBuilder.withCountry(countryNodeExpected).build(ObjectType.NODE);
 
         when(objectMapper.map(targetDTOExpected, TargetNode.class)).thenReturn(targetNodeExpectedBeforeSetCountry);
         when(countryService.findByName(countryDTOExpected.getName())).thenReturn(Optional.of(countryNodeExpected));
@@ -254,10 +260,6 @@ class TargetServiceImplTest {
     @Test
     void when_update_target_should_return_updated_target() {
 
-        Long targetId = 1L;
-
-        String targetName = "Target";
-        String countryName = "Country";
         String updatedTargetName = "Updated Target";
         String updatedCountryName = "Another Country";
 
@@ -265,16 +267,15 @@ class TargetServiceImplTest {
         TargetDTO targetDTOExpected = (TargetDTO) targetBuilder.withTarget(updatedTargetName).withCountry(countryDTOExpected)
                 .build(ObjectType.DTO);
 
-        CountryNode countryNode = (CountryNode) countryBuilder.withName(countryName).build(ObjectType.NODE);
+        CountryNode countryNode = (CountryNode) countryBuilder.build(ObjectType.NODE);
         CountryNode countryNodeExpected = (CountryNode) countryBuilder.withName(updatedCountryName).build(ObjectType.NODE);
 
-        TargetNode targetNodeToUpdate = (TargetNode) targetBuilder.withId(targetId).withTarget(targetName).withCountry(countryNode)
-                .build(ObjectType.NODE);
+        TargetNode targetNodeToUpdate = (TargetNode) targetBuilder.withCountry(countryNode).build(ObjectType.NODE);
         TargetNode targetNodeExpectedBeforeSetCountry = (TargetNode) targetBuilder.withId(null).withTarget(updatedTargetName)
                 .withCountry(null).build(ObjectType.NODE);
         TargetNode targetNodeExpectedBeforeSave = (TargetNode) targetBuilder.withId(null).withTarget(updatedTargetName)
                 .withCountry(countryNodeExpected).build(ObjectType.NODE);
-        TargetNode targetNodeExpected = (TargetNode) targetBuilder.withId(targetId).withTarget(updatedTargetName)
+        TargetNode targetNodeExpected = (TargetNode) targetBuilder.withTarget(updatedTargetName)
                 .withCountry(countryNodeExpected).build(ObjectType.NODE);
 
         when(objectMapper.map(targetDTOExpected, TargetNode.class)).thenReturn(targetNodeExpectedBeforeSetCountry);
@@ -307,12 +308,13 @@ class TargetServiceImplTest {
     @Test
     void when_save_target_should_return_saved_target() {
 
-        Long targetId = 1L;
-        String targetName = "Target";
+        CountryNode countryNodeExpected = (CountryNode) countryBuilder.build(ObjectType.NODE);
+        TargetNode targetNodeExpectedBeforeSave = (TargetNode) targetBuilder.withId(null).withCountry(countryNodeExpected)
+                .build(ObjectType.NODE);
+        TargetNode targetNodeExpected = (TargetNode) targetBuilder.withCountry(countryNodeExpected).build(ObjectType.NODE);
 
-        TargetNode targetNodeExpectedBeforeSave = (TargetNode) targetBuilder.withId(null).withTarget(targetName).build(ObjectType.NODE);
-        TargetNode targetNodeExpected = (TargetNode) targetBuilder.withId(targetId).withTarget(targetName).build(ObjectType.NODE);
-
+        when(countryService.findByName(targetNodeExpectedBeforeSave.getCountryOfOrigin().getName()))
+                .thenReturn(Optional.of(countryNodeExpected));
         when(targetRepository.save(targetNodeExpectedBeforeSave)).thenReturn(targetNodeExpected);
 
         TargetNode targetNodeActual = targetService.save(targetNodeExpectedBeforeSave);
@@ -323,25 +325,34 @@ class TargetServiceImplTest {
                                 + targetNodeActual.getTarget()),
                 () -> assertNotNull(targetNodeActual.getId(),
                         () -> "should return target node with new id, but was: " + targetNodeActual.getId()),
+                () -> assertEquals(targetNodeExpected.getCountryOfOrigin(), targetNodeActual.getCountryOfOrigin(),
+                        () -> "should return target node with country: " + targetNodeExpected.getCountryOfOrigin() + ", but was: " + targetNodeActual.getCountryOfOrigin()),
+                () -> assertEquals(targetNodeExpected.getCountryOfOrigin().getId(), targetNodeActual.getCountryOfOrigin().getId(),
+                        () -> "should return target node with country id: " + targetNodeExpected.getCountryOfOrigin().getId()
+                                + ", but was: " + targetNodeActual.getId()),
+                () -> assertEquals(targetNodeExpected.getCountryOfOrigin().getName(), targetNodeActual.getCountryOfOrigin().getName(),
+                        () -> "should return target node with country name: " + targetNodeExpected.getCountryOfOrigin().getName()
+                                + ", but was: " + targetNodeActual.getCountryOfOrigin()),
                 () -> verify(targetRepository, times(1)).save(targetNodeExpectedBeforeSave),
                 () -> verifyNoMoreInteractions(targetRepository),
-                () -> verifyNoInteractions(objectMapper),
-                () -> verifyNoInteractions(countryService));
+                () -> verify(countryService, times(1))
+                        .findByName(targetNodeExpectedBeforeSave.getCountryOfOrigin().getName()),
+                () -> verifyNoMoreInteractions(countryService),
+                () -> verifyNoInteractions(objectMapper));
     }
 
     @Test
     void when_delete_target_by_id_target_should_delete_and_return_target() {
 
-        String targetName = "Target";
+        Long expectedTargetId = 1L;
 
-        Long targetId = 1L;
-
-        TargetNode targetNodeExpected = (TargetNode) targetBuilder.withId(targetId).withTarget(targetName)
+        CountryNode countryNodeExpected = (CountryNode) countryBuilder.build(ObjectType.NODE);
+        TargetNode targetNodeExpected = (TargetNode) targetBuilder.withId(expectedTargetId).withCountry(countryNodeExpected)
                 .build(ObjectType.NODE);
 
-        when(targetRepository.findById(targetId)).thenReturn(Optional.of(targetNodeExpected));
+        when(targetRepository.findById(expectedTargetId)).thenReturn(Optional.of(targetNodeExpected));
 
-        Optional<TargetNode> targetNodeOptional = targetService.delete(targetId);
+        Optional<TargetNode> targetNodeOptional = targetService.delete(expectedTargetId);
 
         TargetNode targetNodeActual = targetNodeOptional.get();
 
@@ -349,7 +360,15 @@ class TargetServiceImplTest {
                 () -> assertEquals(targetNodeExpected.getTarget(), targetNodeActual.getTarget(),
                         () -> "should return target node with target: " + targetNodeExpected.getTarget() + ", but was: "
                                 + targetNodeActual.getTarget()),
-                () -> verify(targetRepository, times(1)).findById(targetId),
+                () -> assertEquals(targetNodeExpected.getCountryOfOrigin(), targetNodeActual.getCountryOfOrigin(),
+                        () -> "should return target node with country: " + targetNodeExpected.getCountryOfOrigin() + ", but was: " + targetNodeActual.getCountryOfOrigin()),
+                () -> assertEquals(targetNodeExpected.getCountryOfOrigin().getId(), targetNodeActual.getCountryOfOrigin().getId(),
+                        () -> "should return target node with country id: " + targetNodeExpected.getCountryOfOrigin().getId()
+                                + ", but was: " + targetNodeActual.getId()),
+                () -> assertEquals(targetNodeExpected.getCountryOfOrigin().getName(), targetNodeActual.getCountryOfOrigin().getName(),
+                        () -> "should return target node with country name: " + targetNodeExpected.getCountryOfOrigin().getName()
+                                + ", but was: " + targetNodeActual.getCountryOfOrigin()),
+                () -> verify(targetRepository, times(1)).findById(expectedTargetId),
                 () -> verify(targetRepository, times(1)).delete(targetNodeActual),
                 () -> verifyNoMoreInteractions(targetRepository),
                 () -> verifyNoInteractions(objectMapper),
@@ -359,16 +378,16 @@ class TargetServiceImplTest {
     @Test
     void when_delete_target_by_id_not_existing_target_should_return_empty_optional() {
 
-        Long targetId = 1L;
+        Long expectedTargetId = 1L;
 
-        when(targetRepository.findById(targetId)).thenReturn(Optional.empty());
+        when(targetRepository.findById(expectedTargetId)).thenReturn(Optional.empty());
 
-        Optional<TargetNode> targetNodeOptional = targetService.delete(targetId);
+        Optional<TargetNode> targetNodeOptional = targetService.delete(expectedTargetId);
 
         assertAll(
                 () -> assertTrue(targetNodeOptional.isEmpty(),
                         () -> "should return empty target node optional, but was: " + targetNodeOptional.get()),
-                () -> verify(targetRepository, times(1)).findById(targetId),
+                () -> verify(targetRepository, times(1)).findById(expectedTargetId),
                 () -> verifyNoMoreInteractions(targetRepository),
                 () -> verifyNoInteractions(objectMapper),
                 () -> verifyNoInteractions(countryService));
