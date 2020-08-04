a1ea996d39b2b49def2b939dff3442d8d6832243
NowakArtur97 <nowart97@gmail.com>
2020-08-04 10:10:56 +0200

    Create City Model

---
+++ src/main/java/com/NowakArtur97/GlobalTerrorismAPI/model/response/CityModel.java
@@ -0,0 +1,29 @@
+package com.NowakArtur97.GlobalTerrorismAPI.model.response;
+
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.*;
+import org.springframework.hateoas.RepresentationModel;
+
+@ApiModel(description = "Details about the City")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = false)
+@Builder
+public class CityModel extends RepresentationModel<CityModel> {
+
+    @ApiModelProperty(notes = "The unique id of the City")
+    private Long id;
+
+    @ApiModelProperty(notes = "The city's name")
+    private String name;
+
+    @ApiModelProperty(notes = "The city's latitude")
+    private double latitude;
+
+    @ApiModelProperty(notes = "The city's longitude")
+    private double longitude;
+
+}
