5b40fba28a9652eea74a003a9097976708c8f5e2
NowakArtur97 <nowart97@gmail.com>
2020-08-04 08:45:26 +0200

    Create City Repository

---
+++ src/main/java/com/NowakArtur97/GlobalTerrorismAPI/repository/CityRepository.java
@@ -0,0 +1,7 @@
+package com.NowakArtur97.GlobalTerrorismAPI.repository;
+
+import com.NowakArtur97.GlobalTerrorismAPI.node.CityNode;
+
+public interface CityRepository extends BaseRepository<CityNode> {
+
+}
