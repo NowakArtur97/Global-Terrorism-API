c454ac0a6297605850b50875a698066aa5571ecb
NowakArtur97 <nowart97@gmail.com>
2020-08-08 09:22:05 +0200

    Add assertions in Event Model Assembler Test

--- src/test/java/com/NowakArtur97/GlobalTerrorismAPI/assembler/EventModelAssemblerTest.java
+++ src/test/java/com/NowakArtur97/GlobalTerrorismAPI/assembler/EventModelAssemblerTest.java
@@ -1,10 +1,13 @@
 package com.NowakArtur97.GlobalTerrorismAPI.assembler;
 
 import com.NowakArtur97.GlobalTerrorismAPI.mapper.ObjectMapper;
+import com.NowakArtur97.GlobalTerrorismAPI.model.response.CityModel;
 import com.NowakArtur97.GlobalTerrorismAPI.model.response.EventModel;
 import com.NowakArtur97.GlobalTerrorismAPI.model.response.TargetModel;
+import com.NowakArtur97.GlobalTerrorismAPI.node.CityNode;
 import com.NowakArtur97.GlobalTerrorismAPI.node.EventNode;
 import com.NowakArtur97.GlobalTerrorismAPI.node.TargetNode;
+import com.NowakArtur97.GlobalTerrorismAPI.testUtil.builder.CityBuilder;
 import com.NowakArtur97.GlobalTerrorismAPI.testUtil.builder.EventBuilder;
 import com.NowakArtur97.GlobalTerrorismAPI.testUtil.builder.TargetBuilder;
 import com.NowakArtur97.GlobalTerrorismAPI.testUtil.builder.enums.ObjectType;
@@ -34,12 +37,14 @@ class EventModelAssemblerTest {
     private ObjectMapper objectMapper;
 
     private static TargetBuilder targetBuilder;
+    private static CityBuilder cityBuilder;
     private static EventBuilder eventBuilder;
 
     @BeforeAll
     private static void setUpBuilders() {
 
         targetBuilder = new TargetBuilder();
+        cityBuilder = new CityBuilder();
         eventBuilder = new EventBuilder();
     }
 
@@ -54,9 +59,11 @@ class EventModelAssemblerTest {
 
         Long targetId = 1L;
         TargetNode targetNode = (TargetNode) targetBuilder.build(ObjectType.NODE);
-        EventNode eventNode = (EventNode) eventBuilder.withTarget(targetNode).build(ObjectType.NODE);
+        CityNode cityNode = (CityNode) cityBuilder.build(ObjectType.NODE);
+        EventNode eventNode = (EventNode) eventBuilder.withTarget(targetNode).withCity(cityNode).build(ObjectType.NODE);
         TargetModel targetModel = (TargetModel) targetBuilder.build(ObjectType.MODEL);
-        EventModel eventModel = (EventModel) eventBuilder.withTarget(targetModel).build(ObjectType.MODEL);
+        CityModel cityModel = (CityModel) cityBuilder.build(ObjectType.MODEL);
+        EventModel eventModel = (EventModel) eventBuilder.withTarget(targetModel).withCity(cityModel).build(ObjectType.MODEL);
         String pathToLink = BASE_PATH + targetId.intValue();
         Link link = new Link(pathToLink);
         targetModel.add(link);
@@ -92,6 +99,30 @@ class EventModelAssemblerTest {
                 () -> assertEquals(targetModel, model.getTarget(),
                         () -> "should return event model with target model: " + targetModel + ", but was: "
                                 + model.getTarget()),
+                () -> assertEquals(targetModel.getId(), model.getTarget().getId(),
+                        () -> "should return event model with target model id: " + targetModel.getId() + ", but was: "
+                                + model.getTarget().getId()),
+                () -> assertEquals(targetModel.getTarget(), model.getTarget().getTarget(),
+                        () -> "should return event model with target model name: " + targetModel.getTarget() + ", but was: "
+                                + model.getTarget().getTarget()),
+                () -> assertNotNull(eventNode.getCity(),
+                        () -> "should return event model with not null city, but was: null"),
+                () -> assertEquals(cityModel.getId(), model.getCity().getId(),
+                        () -> "should return event model with city model id: " + cityModel.getId() + ", but was: "
+                                + model.getCity().getId()),
+                () -> assertEquals(cityModel.getName(), model.getCity().getName(),
+                        () -> "should return event model with target city name: " + cityModel.getName() + ", but was: "
+                                + model.getCity().getName()),
+                () -> assertEquals(cityModel.getLatitude(), model.getCity().getLatitude(),
+                        () -> "should return event model with target city latitude: " + cityModel.getLatitude() + ", but was: "
+                                + model.getCity().getLatitude()),
+                () -> assertEquals(cityModel.getLongitude(), model.getCity().getLongitude(),
+                        () -> "should return event model with target city longitude: " + cityModel.getLongitude() + ", but was: "
+                                + model.getCity().getLongitude()),
+                () -> assertTrue(model.getCity().getLinks().isEmpty(),
+                        () -> "should return event model with city model with links, but wasn't"),
+                () -> assertFalse(model.getTarget().getLinks().isEmpty(),
+                        () -> "should return event model with target model with links, but wasn't"),
                 () -> assertNotNull(model.getLinks(), () -> "should return model with links, but was: " + model),
                 () -> assertFalse(model.getLinks().isEmpty(),
                         () -> "should return model with links, but was: " + model),
@@ -104,9 +135,11 @@ class EventModelAssemblerTest {
     @Test
     void when_map_event_node_to_model_without_target_should_return_event_model_without_target() {
 
-        EventNode eventNode = (EventNode) eventBuilder.build(ObjectType.NODE);
+        CityNode cityNode = (CityNode) cityBuilder.build(ObjectType.NODE);
+        EventNode eventNode = (EventNode) eventBuilder.withCity(cityNode).build(ObjectType.NODE);
 
-        EventModel eventModel = (EventModel) eventBuilder.build(ObjectType.MODEL);
+        CityModel cityModel = (CityModel) cityBuilder.build(ObjectType.MODEL);
+        EventModel eventModel = (EventModel) eventBuilder.withCity(cityModel).build(ObjectType.MODEL);
 
         when(objectMapper.map(eventNode, EventModel.class)).thenReturn(eventModel);
 
@@ -135,6 +168,22 @@ class EventModelAssemblerTest {
                                 + model.getIsSuicidal()),
                 () -> assertNull(eventNode.getTarget(),
                         () -> "should return event model with null target, but wasn't: null"),
+                () -> assertNotNull(eventNode.getCity(),
+                        () -> "should return event model with not null city, but was: null"),
+                () -> assertEquals(cityModel.getId(), model.getCity().getId(),
+                        () -> "should return event model with city model id: " + cityModel.getId() + ", but was: "
+                                + model.getCity().getId()),
+                () -> assertEquals(cityModel.getName(), model.getCity().getName(),
+                        () -> "should return event model with target city name: " + cityModel.getName() + ", but was: "
+                                + model.getCity().getName()),
+                () -> assertEquals(cityModel.getLatitude(), model.getCity().getLatitude(),
+                        () -> "should return event model with target city latitude: " + cityModel.getLatitude() + ", but was: "
+                                + model.getCity().getLatitude()),
+                () -> assertEquals(cityModel.getLongitude(), model.getCity().getLongitude(),
+                        () -> "should return event model with target city longitude: " + cityModel.getLongitude() + ", but was: "
+                                + model.getCity().getLongitude()),
+                () -> assertTrue(model.getCity().getLinks().isEmpty(),
+                        () -> "should return event model with city model with links, but wasn't"),
                 () -> assertNotNull(model.getLinks(), () -> "should return model with links, but was: " + model),
                 () -> assertFalse(model.getLinks().isEmpty(),
                         () -> "should return model with links, but was: " + model),
