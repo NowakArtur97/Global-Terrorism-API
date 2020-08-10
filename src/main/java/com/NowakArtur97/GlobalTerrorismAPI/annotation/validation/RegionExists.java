eccc912ab805bdfa26f38238f772e4c52290cf42
NowakArtur97 <nowart97@gmail.com>
2020-08-10 08:56:07 +0200

    Create Region Exists annotation

---
+++ src/main/java/com/NowakArtur97/GlobalTerrorismAPI/annotation/validation/RegionExists.java
@@ -0,0 +1,26 @@
+package com.NowakArtur97.GlobalTerrorismAPI.annotation.validation;
+
+import com.NowakArtur97.GlobalTerrorismAPI.validator.CountryExistsConstraintValidator;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Documented
+@Constraint(validatedBy = RegionExistsConstraintValidator.class)
+@Target({FIELD, ANNOTATION_TYPE})
+@Retention(RUNTIME)
+public @interface RegionExists {
+
+    String message() default "A region with the given name does not exist.";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
