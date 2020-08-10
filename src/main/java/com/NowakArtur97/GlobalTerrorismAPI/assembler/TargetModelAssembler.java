0ca5b7d846fdf7f31b6cb5387d34af12d5d469e1
NowakArtur97 <nowart97@gmail.com>
2020-08-10 09:20:54 +0200

    Set Region in Target Model Asseembler

--- src/main/java/com/NowakArtur97/GlobalTerrorismAPI/assembler/TargetModelAssembler.java
+++ src/main/java/com/NowakArtur97/GlobalTerrorismAPI/assembler/TargetModelAssembler.java
@@ -3,7 +3,9 @@ package com.NowakArtur97.GlobalTerrorismAPI.assembler;
 import com.NowakArtur97.GlobalTerrorismAPI.controller.target.TargetController;
 import com.NowakArtur97.GlobalTerrorismAPI.mapper.ObjectMapper;
 import com.NowakArtur97.GlobalTerrorismAPI.model.response.CountryModel;
+import com.NowakArtur97.GlobalTerrorismAPI.model.response.RegionModel;
 import com.NowakArtur97.GlobalTerrorismAPI.model.response.TargetModel;
+import com.NowakArtur97.GlobalTerrorismAPI.node.CountryNode;
 import com.NowakArtur97.GlobalTerrorismAPI.node.TargetNode;
 import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
 import org.springframework.stereotype.Component;
@@ -27,8 +29,15 @@ public class TargetModelAssembler extends RepresentationModelAssemblerSupport<Ta
 
         TargetModel targetModel = objectMapper.map(targetNode, TargetModel.class);
 
-        if (targetNode.getCountryOfOrigin() != null) {
-            targetModel.setCountryOfOrigin(objectMapper.map(targetNode.getCountryOfOrigin(), CountryModel.class));
+        CountryNode countryNode = targetNode.getCountryOfOrigin();
+
+        if (countryNode != null) {
+            targetModel.setCountryOfOrigin(objectMapper.map(countryNode, CountryModel.class));
+
+            if (countryNode.getRegion() != null) {
+                targetModel.getCountryOfOrigin().setRegion(
+                        objectMapper.map(countryNode.getRegion(), RegionModel.class));
+            }
         }
 
         targetModel.add(linkTo(methodOn(TargetController.class).findById(targetModel.getId())).withSelfRel());
