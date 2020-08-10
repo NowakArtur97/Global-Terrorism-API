f2255d092fb27dbde29c162e932fbe6eebfdede1
NowakArtur97 <nowart97@gmail.com>
2020-08-10 10:07:07 +0200

    Create Neo4j Test Configuration

---
+++ src/test/java/com/NowakArtur97/GlobalTerrorismAPI/testUtil/configuration/Neo4jTestConfiguration.java
@@ -0,0 +1,27 @@
+package com.NowakArtur97.GlobalTerrorismAPI.testUtil.configuration;
+
+import org.neo4j.driver.AuthTokens;
+import org.neo4j.driver.Driver;
+import org.neo4j.driver.GraphDatabase;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+
+@TestConfiguration
+public class Neo4jTestConfiguration {
+
+    @Value("${spring.data.neo4j.uri}")
+    private String uri;
+
+    @Value("${spring.data.neo4j.username}")
+    private String username;
+
+    @Value("${spring.data.neo4j.password}")
+    private String password;
+
+    @Bean
+    public Driver neo4jDriver() {
+
+        return GraphDatabase.driver(uri, AuthTokens.basic(username, password));
+    }
+}
