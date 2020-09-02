cc46a56a17c80709e881318594688906ff0c5633
NowakArtur97 <nowart97@gmail.com>
2020-09-02 19:29:07 +0200

    Configure Model Mapper mapping between Json Value and Country Node

--- src/main/java/com/NowakArtur97/GlobalTerrorismAPI/configuration/ModelMapperConfiguration.java
+++ src/main/java/com/NowakArtur97/GlobalTerrorismAPI/configuration/ModelMapperConfiguration.java
@@ -1,9 +1,13 @@
 package com.NowakArtur97.GlobalTerrorismAPI.configuration;
 
+import com.NowakArtur97.GlobalTerrorismAPI.node.CountryNode;
+import org.modelmapper.Converter;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.json.JsonValue;
 
 @Configuration
 public class ModelMapperConfiguration {
@@ -14,6 +18,17 @@ public class ModelMapperConfiguration {
 		ModelMapper modelMapper = new ModelMapper();
 
 		modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.LOOSE);
+
+		Converter<JsonValue, CountryNode> myConverter = context -> {
+
+			CountryNode destination = context.getDestination();
+
+			destination.setRegion(destination.getRegion());
+
+			return destination;
+		};
+
+		modelMapper.addConverter(myConverter);
 
 		return modelMapper;
 	}
