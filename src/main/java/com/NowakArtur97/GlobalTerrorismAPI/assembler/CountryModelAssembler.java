1943a4d2c8c21574a6dc7fc9d2cace1a94a5279a
NowakArtur97 <nowart97@gmail.com>
2020-09-07 10:27:52 +0200

    Add Region Model Assembler dependency in in Country Model Assembler

--- src/main/java/com/NowakArtur97/GlobalTerrorismAPI/assembler/CountryModelAssembler.java
+++ src/main/java/com/NowakArtur97/GlobalTerrorismAPI/assembler/CountryModelAssembler.java
@@ -13,18 +13,25 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 @Component
 public class CountryModelAssembler extends RepresentationModelAssemblerSupport<CountryNode, CountryModel> {
 
+    private final RegionModelAssembler regionModelAssembler;
+
     private final ObjectMapper objectMapper;
 
-    public CountryModelAssembler(ObjectMapper objectMapper) {
+    public CountryModelAssembler(RegionModelAssembler regionModelAssembler, ObjectMapper objectMapper) {
 
         super(CountryController.class, CountryModel.class);
         this.objectMapper = objectMapper;
+        this.regionModelAssembler = regionModelAssembler;
     }
 
     @Override
     public CountryModel toModel(CountryNode countryNode) {
 
         CountryModel countryModel = objectMapper.map(countryNode, CountryModel.class);
+
+        if (countryNode.getRegion() != null) {
+            countryModel.setRegion(regionModelAssembler.toModel(countryNode.getRegion()));
+        }
 
         countryModel.add(linkTo(methodOn(CountryController.class).findById(countryModel.getId())).withSelfRel());
 
