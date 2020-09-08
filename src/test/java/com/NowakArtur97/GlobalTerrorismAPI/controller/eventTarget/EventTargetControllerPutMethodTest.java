c238e14d4c28ea87ee5abfb8daa5287fc9e9428f
NowakArtur97 <nowart97@gmail.com>
2020-09-08 07:37:41 +0200

    Refactor Event Target Controller Put Method Test

--- src/test/java/com/NowakArtur97/GlobalTerrorismAPI/controller/eventTarget/EventTargetControllerPutMethodTest.java
+++ src/test/java/com/NowakArtur97/GlobalTerrorismAPI/controller/eventTarget/EventTargetControllerPutMethodTest.java
@@ -46,6 +46,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @Tag("TargetController_Tests")
 class EventTargetControllerPutMethodTest {
 
+    private final String REGION_BASE_PATH = "http://localhost:8080/api/v1/regions";
     private final String COUNTRY_BASE_PATH = "http://localhost:8080/api/v1/countries";
     private final String TARGET_BASE_PATH = "http://localhost:8080/api/v1/targets";
     private final String EVENT_BASE_PATH = "http://localhost:8080/api/v1/events";
@@ -111,6 +112,7 @@ class EventTargetControllerPutMethodTest {
         CountryDTO countryDTO = (CountryDTO) countryBuilder.withName(countryNode.getName()).build(ObjectType.DTO);
         TargetDTO targetDTO = (TargetDTO) targetBuilder.withCountry(countryDTO).build(ObjectType.DTO);
 
+        String pathToRegionLink = REGION_BASE_PATH + "/" + regionNode.getId().intValue();
         String pathToCountryLink = COUNTRY_BASE_PATH + "/" + countryNode.getId().intValue();
 
         String token = jwtUtil.generateToken(new User(userNode.getUserName(), userNode.getPassword(),
@@ -131,9 +133,10 @@ class EventTargetControllerPutMethodTest {
                         .andExpect(jsonPath("countryOfOrigin.links[1].href").doesNotExist())
                         .andExpect(jsonPath("countryOfOrigin.id", is(countryNode.getId().intValue())))
                         .andExpect(jsonPath("countryOfOrigin.name", is(countryNode.getName())))
+                        .andExpect(jsonPath("countryOfOrigin.region.links[0].href", is(pathToRegionLink)))
+                        .andExpect(jsonPath("countryOfOrigin.region.links[1].href").doesNotExist())
                         .andExpect(jsonPath("countryOfOrigin.region.id", is(regionNode.getId().intValue())))
-                        .andExpect(jsonPath("countryOfOrigin.region.name", is(regionNode.getName())))
-                        .andExpect(jsonPath("countryOfOrigin.region.links").isEmpty()));
+                        .andExpect(jsonPath("countryOfOrigin.region.name", is(regionNode.getName()))));
     }
 
     @Test
@@ -143,6 +146,7 @@ class EventTargetControllerPutMethodTest {
         CountryDTO countryDTO = (CountryDTO) countryBuilder.withName(anotherCountryNode.getName()).build(ObjectType.DTO);
         TargetDTO targetDTO = (TargetDTO) targetBuilder.withTarget(updatedTargetName).withCountry(countryDTO).build(ObjectType.DTO);
 
+        String pathToRegionLink = REGION_BASE_PATH + "/" + anotherRegionNode.getId().intValue();
         String pathToCountryLink = COUNTRY_BASE_PATH + "/" + anotherCountryNode.getId().intValue();
         String pathToLink = TARGET_BASE_PATH + "/" + targetNode.getId().intValue();
 
@@ -164,9 +168,10 @@ class EventTargetControllerPutMethodTest {
                         .andExpect(jsonPath("countryOfOrigin.links[1].href").doesNotExist())
                         .andExpect(jsonPath("countryOfOrigin.id", is(anotherCountryNode.getId().intValue())))
                         .andExpect(jsonPath("countryOfOrigin.name", is(anotherCountryNode.getName())))
+                        .andExpect(jsonPath("countryOfOrigin.region.links[0].href", is(pathToRegionLink)))
+                        .andExpect(jsonPath("countryOfOrigin.region.links[1].href").doesNotExist())
                         .andExpect(jsonPath("countryOfOrigin.region.id", is(anotherRegionNode.getId().intValue())))
-                        .andExpect(jsonPath("countryOfOrigin.region.name", is(anotherRegionNode.getName())))
-                        .andExpect(jsonPath("countryOfOrigin.region.links").isEmpty()));
+                        .andExpect(jsonPath("countryOfOrigin.region.name", is(anotherRegionNode.getName()))));
     }
 
     @ParameterizedTest(name = "{index}: Target Name: {0}")
