4eb83537bfbef622c73a02190d21f274fd1e6be7
NowakArtur97 <nowart97@gmail.com>
2020-09-17 11:13:44 +0200

    Enable CORS in Web Security Configuration

--- server/src/main/java/com/nowakArtur97/globalTerrorismAPI/configuration/WebSecurityConfiguration.java
+++ server/src/main/java/com/nowakArtur97/globalTerrorismAPI/configuration/WebSecurityConfiguration.java
@@ -1,8 +1,8 @@
 package com.nowakArtur97.globalTerrorismAPI.configuration;
 
+import com.nowakArtur97.globalTerrorismAPI.feature.user.CustomUserDetailsService;
 import com.nowakArtur97.globalTerrorismAPI.filter.ExceptionHandlerFilter;
 import com.nowakArtur97.globalTerrorismAPI.filter.JwtRequestFilter;
-import com.nowakArtur97.globalTerrorismAPI.feature.user.CustomUserDetailsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -16,7 +16,13 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
@@ -56,10 +62,25 @@ class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
         return super.authenticationManagerBean();
     }
 
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:4200/"));
+        configuration.setAllowedMethods(Collections.singletonList("GET"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-        http.csrf().disable()
+        http
+                .cors()
+                .and()
+                .csrf().disable()
                 .authorizeRequests()
                 .antMatchers(ignoredAntMatchers)
                 .permitAll()
