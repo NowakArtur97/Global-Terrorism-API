76d45fb59a7a174d7475446ac242aa55872921f4
NowakArtur97 <nowart97@gmail.com>
2020-09-18 08:50:58 +0200

    Configure Leaflet styles and assets

--- client/src/app/cities/cities.module.ts
+++ client/src/app/cities/cities.module.ts
@@ -1,5 +1,6 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 import { StoreModule } from '@ngrx/store';
 
 import { CitiesComponent } from './cities.component';
@@ -7,7 +8,11 @@ import citiesReducer from './store/cities.reducer';
 
 @NgModule({
   declarations: [CitiesComponent],
-  imports: [HttpClientModule, StoreModule.forFeature('cities', citiesReducer)],
+  imports: [
+    HttpClientModule,
+    StoreModule.forFeature('cities', citiesReducer),
+    LeafletModule,
+  ],
   exports: [CitiesComponent],
 })
 export default class CitiesModule {}
