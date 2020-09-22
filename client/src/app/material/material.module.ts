f1f5c309b98f6eee856c71e42d32574261691fe8
NowakArtur97 <nowart97@gmail.com>
2020-09-22 08:05:49 +0200

    Create simple login form using Angular Material

--- client/src/app/material/material.module.ts
+++ client/src/app/material/material.module.ts
@@ -2,7 +2,10 @@ import { LayoutModule } from '@angular/cdk/layout';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -13,10 +16,13 @@ import { NavigationComponent } from './navigation/navigation.component';
 const materialComponents = [
   LayoutModule,
   MatToolbarModule,
-  MatButtonModule,
   MatSidenavModule,
   MatListModule,
   MatDialogModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatButtonModule,
+  MatCardModule,
 ];
 
 @NgModule({
