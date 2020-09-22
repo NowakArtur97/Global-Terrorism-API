c70f5e34687dddd09f941ab971ded4b059dce650
NowakArtur97 <nowart97@gmail.com>
2020-09-22 12:04:12 +0200

    Add constructor and getters in Error Response

--- client/src/app/shared/models/ErrorResponse.ts
+++ client/src/app/shared/models/ErrorResponse.ts
@@ -1,5 +1,19 @@
 export default class ErrorResponse {
-  errors: string[];
-  status: number;
-  timestamp: Date;
+  constructor(
+    private readonly _errors: string[],
+    private readonly _status: number,
+    private readonly _timestamp: Date
+  ) {}
+
+  public get errors(): string[] {
+    return this._errors;
+  }
+
+  public get status(): number {
+    return this._status;
+  }
+
+  public get timestamp(): Date {
+    return this._timestamp;
+  }
 }
