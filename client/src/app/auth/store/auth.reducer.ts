61b8f4236be84a72b8b569eed6182c89fcd53ec7
NowakArtur97 <nowart97@gmail.com>
2020-09-22 08:54:03 +0200

    Create Auth Reducer

---
+++ client/src/app/auth/store/auth.reducer.ts
@@ -0,0 +1,25 @@
+import { Action, createReducer, on } from '@ngrx/store';
+
+import * as AuthActions from './auth.actions';
+import AuthStoreState from './auth.store.state';
+
+const initialState: AuthStoreState = {
+  user: null,
+};
+
+const _authReducer = createReducer(
+  initialState,
+  on(AuthActions.loginUser, (state, action) => ({
+    ...state,
+    user: action.user,
+  })),
+
+  on(AuthActions.logoutUser, (state, action) => ({
+    ...state,
+    user: null,
+  }))
+);
+
+export default function authReducer(state: AuthStoreState, action: Action) {
+  return _authReducer(state, action);
+}
