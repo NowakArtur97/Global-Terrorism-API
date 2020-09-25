4f4bb7b2c5436f313c601481fc24e1b1a67c8b3c
NowakArtur97 <nowart97@gmail.com>
2020-09-25 08:57:56 +0200

    Create Cities Effects test

---
+++ client/src/app/cities/store/tests/cities.effects.spec.ts
@@ -0,0 +1,53 @@
+import { TestBed } from '@angular/core/testing';
+import { provideMockActions } from '@ngrx/effects/testing';
+import { of, ReplaySubject } from 'rxjs';
+
+import CitiesGetResponse from '../../models/cities-get-response.model';
+import City from '../../models/city.model';
+import CitiesService from '../../services/cities.service';
+import * as CitiesActions from '../cities.actions';
+import CitiesEffects from '../cities.effects';
+
+const mockCities: CitiesGetResponse = {
+  content: [new City(1, 'city1', 10, 12), new City(2, 'city2', 20, 22)],
+};
+
+describe('CitiesEffects', () => {
+  let citiesEffects: CitiesEffects;
+  let actions$: ReplaySubject<any>;
+  let citiesService: CitiesService;
+
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      providers: [
+        CitiesEffects,
+        provideMockActions(() => actions$),
+        {
+          provide: CitiesService,
+          useValue: jasmine.createSpyObj('citiesService', ['getCities']),
+        },
+      ],
+    })
+  );
+
+  beforeEach(() => {
+    citiesEffects = TestBed.get(CitiesEffects);
+    citiesService = TestBed.get(CitiesService);
+    (citiesService.getCities as jasmine.Spy).and.returnValue(of(mockCities));
+  });
+
+  describe('fetchCities$', () => {
+    beforeEach(() => {
+      actions$ = new ReplaySubject(1);
+      actions$.next(CitiesActions.fetchCitites());
+    });
+
+    it('should return a setCities action', () => {
+      citiesEffects.fetchCities$.subscribe((resultAction) => {
+        expect(resultAction).toEqual(
+          CitiesActions.setCities({ cities: mockCities.content })
+        );
+      });
+    });
+  });
+});
