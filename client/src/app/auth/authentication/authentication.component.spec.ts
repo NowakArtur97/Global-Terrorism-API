7002b687989cb141c8f36641e335a36f6fa27ef4
NowakArtur97 <nowart97@gmail.com>
2020-09-26 09:51:29 +0200

    Create test for dispatching action in Authentication Component test

--- client/src/app/auth/authentication/authentication.component.spec.ts
+++ client/src/app/auth/authentication/authentication.component.spec.ts
@@ -1,24 +1,62 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { Store, StoreModule } from '@ngrx/store';
+import { of } from 'rxjs';
+import AppStoreState from 'src/app/store/app.store.state';
 
+import LoginData from '../models/LoginData';
+import * as AuthActions from '../store/auth.actions';
 import { AuthenticationComponent } from './authentication.component';
 
 describe('AuthenticationComponent', () => {
   let component: AuthenticationComponent;
   let fixture: ComponentFixture<AuthenticationComponent>;
+  let store: Store<AppStoreState>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [StoreModule.forRoot({}), ReactiveFormsModule],
       declarations: [AuthenticationComponent],
+      providers: [Store],
     }).compileComponents();
-  });
 
-  beforeEach(() => {
     fixture = TestBed.createComponent(AuthenticationComponent);
     component = fixture.componentInstance;
+
+    store = TestBed.inject(Store);
+    spyOn(store, 'select').and.callFake((selector) => {
+      if (selector === 'auth') {
+        return of([]);
+      }
+    });
+    spyOn(store, 'dispatch');
+
     fixture.detectChanges();
+    component.ngOnInit();
   });
 
-  // it('should create', () => {
-  //   expect(component).toBeTruthy();
-  // });
+  describe('when login form is submitted', () => {
+    it('form invalid when empty', () => {
+      expect(component.loginForm.valid).toBeFalsy();
+    });
+
+    it('should dispatch loginUserStart action when login form is valid', () => {
+      expect(component.loginForm.valid).toBeFalsy();
+
+      const loginData = new LoginData('login', ' password');
+
+      component.loginForm.controls['userNameOrEmail'].setValue(
+        loginData.userNameOrEmail
+      );
+      component.loginForm.controls['password'].setValue(loginData.password);
+
+      expect(component.loginForm.valid).toBeTruthy();
+
+      component.onLogin();
+
+      expect(store.dispatch).toHaveBeenCalledWith(
+        AuthActions.loginUserStart({ loginData })
+      );
+    });
+  });
 });
--- client/src/app/map/map.component.spec.ts
+++ client/src/app/map/map.component.spec.ts
@@ -1,24 +1,35 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+// import { ComponentFixture, TestBed } from '@angular/core/testing';
+// import { Store, StoreModule } from '@ngrx/store';
 
-import { MapComponent } from './map.component';
+// import AppStoreState from '../store/app.store.state';
+// import { MapComponent } from './map.component';
 
-describe('MapComponent', () => {
-  let component: MapComponent;
-  let fixture: ComponentFixture<MapComponent>;
+// describe('MapComponent', () => {
+//   let component: MapComponent;
+//   let fixture: ComponentFixture<MapComponent>;
+//   let store: Store<AppStoreState>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [MapComponent],
-    }).compileComponents();
-  });
+//   beforeEach(async () => {
+//     await TestBed.configureTestingModule({
+//       imports: [StoreModule.forRoot({})],
+//       declarations: [MapComponent],
+//       providers: [Store],
+//     }).compileComponents();
+//   });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(MapComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+//   beforeEach(() => {
+//     fixture = TestBed.createComponent(MapComponent);
+//     component = fixture.componentInstance;
 
-  // it('should create', () => {
-  //   expect(component).toBeTruthy();
-  // });
-});
+//     store = TestBed.inject(Store);
+//     spyOn(store, 'dispatch');
+
+//     fixture.detectChanges();
+//   });
+
+//   describe('when fetch cars button is clickedcomponent is initialized', () => {
+//     it('should dispatch FetchCars action', () => {
+//       expect(store.dispatch).toHaveBeenCalledWith(new FetchCars()); // czy dispatch zawołany z dobrą akcją
+//     });
+//   });
+// });
