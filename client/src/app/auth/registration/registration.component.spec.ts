2a4cf4efc8c2eaf2d2b1301182bbf078956e0311
NowakArtur97 <nowart97@gmail.com>
2020-09-26 09:58:11 +0200

    Create test for dispatching action in Registration Component test

--- client/src/app/auth/registration/registration.component.spec.ts
+++ client/src/app/auth/registration/registration.component.spec.ts
@@ -1,24 +1,73 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { Store, StoreModule } from '@ngrx/store';
+import { of } from 'rxjs';
+import AppStoreState from 'src/app/store/app.store.state';
 
+import RegistrationData from '../models/RegistrationData';
+import * as AuthActions from '../store/auth.actions';
 import { RegistrationComponent } from './registration.component';
 
 describe('RegistrationComponent', () => {
   let component: RegistrationComponent;
   let fixture: ComponentFixture<RegistrationComponent>;
+  let store: Store<AppStoreState>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [StoreModule.forRoot({}), ReactiveFormsModule],
       declarations: [RegistrationComponent],
+      providers: [Store],
     }).compileComponents();
-  });
 
-  beforeEach(() => {
     fixture = TestBed.createComponent(RegistrationComponent);
     component = fixture.componentInstance;
+
+    store = TestBed.inject(Store);
+    spyOn(store, 'select').and.callFake((selector) => {
+      if (selector === 'auth') {
+        return of([]);
+      }
+    });
+    spyOn(store, 'dispatch');
+
     fixture.detectChanges();
+    component.ngOnInit();
   });
 
-  // it('should create', () => {
-  //   expect(component).toBeTruthy();
-  // });
+  describe('when register form is submitted', () => {
+    it('form invalid when empty', () => {
+      expect(component.registerForm.valid).toBeFalsy();
+    });
+
+    it('should dispatch registerUserStart action when register form is valid', () => {
+      expect(component.registerForm.valid).toBeFalsy();
+
+      const registrationData = new RegistrationData(
+        'username',
+        'email@email.com',
+        'password',
+        'password'
+      );
+
+      component.registerForm.controls['userName'].setValue(
+        registrationData.userName
+      );
+      component.registerForm.controls['email'].setValue(registrationData.email);
+      component.registerForm.controls['password'].setValue(
+        registrationData.password
+      );
+      component.registerForm.controls['matchingPassword'].setValue(
+        registrationData.matchingPassword
+      );
+
+      expect(component.registerForm.valid).toBeTruthy();
+
+      component.onRegister();
+
+      expect(store.dispatch).toHaveBeenCalledWith(
+        AuthActions.registerUserStart({ registrationData })
+      );
+    });
+  });
 });
