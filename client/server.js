81c69bbbe73bdd415a1b1298ae9366be60bcf258
NowakArtur97 <nowart97@gmail.com>
2020-10-09 17:19:31 +0200

    Change project dist path in server.js

--- client/server.js
+++ client/server.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-
-app.use(express.static(__dirname + "/dist"));
+console.log(__dirname + "/dist/client");
+app.use(express.static(__dirname + "/dist/client"));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/dist/index.html"));
+  res.sendFile(path.join(__dirname + "/dist/client/index.html"));
 });
 
 app.listen(process.env.PORT || 8080);
