5d07e676798307c40a65c5cac99b7ccd82ba89e4
NowakArtur97 <nowart97@gmail.com>
2020-10-09 18:09:09 +0200

    Add postinstall command in package.json

--- client/server.js
+++ client/server.js
@@ -1,13 +1,11 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-console.log(__dirname);
-console.log(path.join(__dirname + "/dist/client/index.html"));
-console.log(path.dirname);
-app.use(express.static("/dist/client"));
+
+app.use(express.static(__dirname + "/dist/client"));
 
 app.get("/*", function (req, res) {
-  res.sendFile("/dist/client/index.html");
+  res.sendFile(path.join(__dirname + "/dist/client/index.html"));
 });
 
 app.listen(process.env.PORT || 3000);
