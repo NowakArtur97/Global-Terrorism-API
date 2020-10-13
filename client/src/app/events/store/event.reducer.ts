68395b9c09768481899f7abcae73e68e23f88da6
NowakArtur97 <nowart97@gmail.com>
2020-10-13 16:29:56 +0200

    Create Event Reducer

---
+++ client/src/app/events/store/event.reducer.ts
@@ -0,0 +1,28 @@
+import { Action, createReducer, on } from '@ngrx/store';
+
+import * as EventActions from './event.actions';
+import EventStoreState from './event.store.state';
+
+const initialState: EventStoreState = {
+  events: [],
+};
+
+const _eventReducer = createReducer(
+  initialState,
+  on(EventActions.setEvents, (state, action) => ({
+    ...state,
+    events: [...action.events],
+  })),
+
+  on(EventActions.resetEvents, (state) => ({
+    ...state,
+    cities: [],
+  }))
+);
+
+export default function citiesReducer(
+  state: EventStoreState,
+  action: Action
+): EventStoreState {
+  return _eventReducer(state, action);
+}
