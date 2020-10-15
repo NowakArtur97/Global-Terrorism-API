a00ea71607423b2f346dd7cd217c07c9cb761544
NowakArtur97 <nowart97@gmail.com>
2020-10-15 16:56:50 +0200

    Refactor Map Component test

--- client/src/app/map/map.component.spec.ts
+++ client/src/app/map/map.component.spec.ts
@@ -3,12 +3,20 @@ import { Store, StoreModule } from '@ngrx/store';
 import { of } from 'rxjs';
 import AppStoreState from 'src/app/store/app.store.state';
 
+import User from '../auth/models/user.model';
+import AuthStoreState from '../auth/store/auth.store.state';
+import * as CityActions from '../city/store/city.actions';
 import { MapComponent } from './map.component';
 
 describe('MapComponent', () => {
   let component: MapComponent;
   let fixture: ComponentFixture<MapComponent>;
   let store: Store<AppStoreState>;
+  const initialStateWithUser: AuthStoreState = {
+    user: new User('token', 36000000),
+    authErrorMessages: [],
+    isLoading: false,
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -27,15 +35,19 @@ describe('MapComponent', () => {
       if (selector === 'city') {
         return of([]);
       }
+      if (selector === 'auth') {
+        return of(initialStateWithUser);
+      }
     });
-
+    spyOn(store, 'dispatch');
     fixture.detectChanges();
     component.ngOnInit();
   });
 
   describe('when initialize component', () => {
-    it('should select cities from store', () => {
+    it('should select and dispatch cities', () => {
       expect(store.select).toHaveBeenCalled();
+      expect(store.dispatch).toHaveBeenCalledWith(CityActions.fetchCities());
     });
   });
 });
