8ca83c002ff9a2dc1eb5e90f25393c1e2c6c3078
NowakArtur97 <nowart97@gmail.com>
2020-10-15 08:56:37 +0200

    Create Abstract Rest Service

---
+++ client/src/app/shared/services/abstract-rest.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+export default abstract class RestAbstractService<T> {
+  private readonly DEFAULT_PAGE_SIZE = 50;
+
+  constructor(protected httpClient: HttpClient, protected actionUtl: string) {}
+
+  get(pageSize: number = this.DEFAULT_PAGE_SIZE): Observable<T> {
+    return this.httpClient.get<T>(
+      `${environment.baseApiUrl}/${this.actionUtl}?page=0&size=${pageSize}`
+    );
+  }
+}
