194e30b95d459eb95c2d197f5a34644794e15d31
NowakArtur97 <nowart97@gmail.com>
2020-10-18 09:53:00 +0200

    Generate Equals and Hash Code methods in Victim Node

--- server/src/main/java/com/nowakArtur97/globalTerrorismAPI/feature/victim/VictimNode.java
+++ server/src/main/java/com/nowakArtur97/globalTerrorismAPI/feature/victim/VictimNode.java
@@ -6,6 +6,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.neo4j.ogm.annotation.NodeEntity;
 
+import java.util.Objects;
+
 @NodeEntity(label = "Victim")
 @Getter
 @Setter
@@ -21,4 +23,24 @@ public class VictimNode extends Node implements Victim {
     private Long numberOfPerpetratorInjured;
 
     private Long valueOfPropertyDamage;
+
+    @Override
+    public boolean equals(Object o) {
+
+        if (this == o) return true;
+        if (!(o instanceof VictimNode)) return false;
+
+        VictimNode that = (VictimNode) o;
+        return Objects.equals(getTotalNumberOfFatalities(), that.getTotalNumberOfFatalities()) &&
+                Objects.equals(getNumberOfPerpetratorFatalities(), that.getNumberOfPerpetratorFatalities()) &&
+                Objects.equals(getTotalNumberOfInjured(), that.getTotalNumberOfInjured()) &&
+                Objects.equals(getNumberOfPerpetratorInjured(), that.getNumberOfPerpetratorInjured()) &&
+                Objects.equals(getValueOfPropertyDamage(), that.getValueOfPropertyDamage());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getTotalNumberOfFatalities(), getNumberOfPerpetratorFatalities(),
+                getTotalNumberOfInjured(), getNumberOfPerpetratorInjured(), getValueOfPropertyDamage());
+    }
 }
