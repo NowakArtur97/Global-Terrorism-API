b023a232a1d50a27570a90bf1478c0e382a317ed
NowakArtur97 <nowart97@gmail.com>
2020-10-18 10:34:54 +0200

    Create Victim Repository

---
+++ server/src/main/java/com/nowakArtur97/globalTerrorismAPI/feature/victim/VictimRepository.java
@@ -0,0 +1,6 @@
+package com.nowakArtur97.globalTerrorismAPI.feature.victim;
+
+import com.nowakArtur97.globalTerrorismAPI.common.repository.BaseRepository;
+
+public interface VictimRepository extends BaseRepository<VictimNode> {
+}
