1e97718a38b216a0ded20c468d3f45c7702aaca5
NowakArtur97 <nowart97@gmail.com>
2020-10-22 16:18:25 +0200

    Create Victim Controller Options Method test

---
+++ server/src/test/java/com/nowakArtur97/globalTerrorismAPI/feature/victim/VictimControllerOptionsMethodTest.java
@@ -0,0 +1,97 @@
+package com.nowakArtur97.globalTerrorismAPI.feature.victim;
+
+import com.nowakArtur97.globalTerrorismAPI.common.controller.BasicGenericRestController;
+import com.nowakArtur97.globalTerrorismAPI.common.service.GenericService;
+import com.nowakArtur97.globalTerrorismAPI.common.util.PatchUtil;
+import com.nowakArtur97.globalTerrorismAPI.common.util.ViolationUtil;
+import com.nowakArtur97.globalTerrorismAPI.testUtil.nameGenerator.NameWithSpacesGenerator;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verifyNoInteractions;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.options;
+
+@ExtendWith(MockitoExtension.class)
+@DisplayNameGeneration(NameWithSpacesGenerator.class)
+@Tag("VictimController_Tests")
+class VictimControllerOptionsMethodTest {
+
+    private final String EVENT_BASE_PATH = "http://localhost:8080/api/v1/victims";
+
+    private MockMvc mockMvc;
+
+    @Mock
+    private GenericService<VictimNode, VictimDTO> victimService;
+
+    @Mock
+    private VictimModelAssembler modelAssembler;
+
+    @Mock
+    private PagedResourcesAssembler<VictimNode> pagedResourcesAssembler;
+
+    @Mock
+    private PatchUtil patchUtil;
+
+    @Mock
+    private ViolationUtil<VictimNode, VictimDTO> violationUtil;
+
+    @BeforeEach
+    private void setUp() {
+
+        BasicGenericRestController<VictimModel> victimController
+                = new VictimController(victimService, modelAssembler, pagedResourcesAssembler);
+
+        mockMvc = MockMvcBuilders.standaloneSetup(victimController).build();
+    }
+
+    @Test
+    @SneakyThrows
+    void when_show_endpoint_options_for_collection_should_show_possible_request_methods() {
+
+        MvcResult mvcResult = mockMvc.perform(options(EVENT_BASE_PATH)).andReturn();
+        String allowedMethods = mvcResult.getResponse().getHeader("allow");
+
+        assertAll(
+                () -> assertNotNull(allowedMethods, () -> "should header contain allowed methods, but wasn't"),
+                () -> assertTrue(allowedMethods.contains("GET"), () -> "should contain GET option, but was: " + allowedMethods),
+                () -> assertTrue(allowedMethods.contains("OPTIONS"), () -> "should contain OPTIONS option, but was: " + allowedMethods),
+                () -> verifyNoInteractions(victimService),
+                () -> verifyNoInteractions(pagedResourcesAssembler),
+                () -> verifyNoInteractions(modelAssembler),
+                () -> verifyNoInteractions(patchUtil),
+                () -> verifyNoInteractions(violationUtil));
+    }
+
+    @Test
+    @SneakyThrows
+    void when_show_endpoint_options_for_singular_resource_should_show_possible_request_methods() {
+
+        Long victimId = 1L;
+
+        String linkWithParameter = EVENT_BASE_PATH + "/" + "{id}";
+
+        MvcResult mvcResult = mockMvc.perform(options(linkWithParameter, victimId)).andReturn();
+        String allowedMethods = mvcResult.getResponse().getHeader("allow");
+
+        assertAll(
+                () -> assertNotNull(allowedMethods, () -> "should header contain allowed methods, but wasn't"),
+                () -> assertTrue(allowedMethods.contains("GET"), () -> "should contain GET option, but was: " + allowedMethods),
+                () -> assertTrue(allowedMethods.contains("OPTIONS"), () -> "should contain OPTIONS option, but was: " + allowedMethods),
+                () -> verifyNoInteractions(victimService),
+                () -> verifyNoInteractions(pagedResourcesAssembler),
+                () -> verifyNoInteractions(modelAssembler),
+                () -> verifyNoInteractions(patchUtil),
+                () -> verifyNoInteractions(violationUtil));
+    }
+}
