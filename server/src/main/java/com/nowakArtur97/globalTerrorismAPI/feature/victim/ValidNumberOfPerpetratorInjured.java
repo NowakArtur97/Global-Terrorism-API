ca45f953a2336fe78d052a3432518a388a11248e
NowakArtur97 <nowart97@gmail.com>
2020-10-23 07:44:25 +0200

    Create Valid Number Of Perpetrator Injured Constraint Validator

--- server/src/main/java/com/nowakArtur97/globalTerrorismAPI/feature/victim/ValidNumberOfPerpetratorInjured.java
+++ server/src/main/java/com/nowakArtur97/globalTerrorismAPI/feature/victim/ValidNumberOfPerpetratorInjured.java
@@ -11,7 +11,7 @@ import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 @Documented
-@Constraint(validatedBy = ValidNumberOfPerpetratorFatalitiesConstraintValidator.class)
+@Constraint(validatedBy = ValidNumberOfPerpetratorInjuredConstraintValidator.class)
 @Target({TYPE, ANNOTATION_TYPE})
 @Retention(RUNTIME)
 public @interface ValidNumberOfPerpetratorInjured {
