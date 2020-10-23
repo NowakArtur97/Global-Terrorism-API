f804dedc128bb52c05a8c00aadab7d113443cd78
NowakArtur97 <nowart97@gmail.com>
2020-10-23 07:39:48 +0200

    Create Valid Number Of Perpetrator Fatalities Constraint Validator

--- server/src/main/java/com/nowakArtur97/globalTerrorismAPI/feature/victim/ValidNumberOfPerpetratorFatalities.java
+++ server/src/main/java/com/nowakArtur97/globalTerrorismAPI/feature/victim/ValidNumberOfPerpetratorFatalities.java
@@ -11,7 +11,7 @@ import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 @Documented
-@Constraint(validatedBy = ValidNumberOfPerpetratorFatalitiesConstraintValdiator.class)
+@Constraint(validatedBy = ValidNumberOfPerpetratorFatalitiesConstraintValidator.class)
 @Target({TYPE, ANNOTATION_TYPE})
 @Retention(RUNTIME)
 public @interface ValidNumberOfPerpetratorFatalities {
---
+++ server/src/main/java/com/nowakArtur97/globalTerrorismAPI/feature/victim/ValidNumberOfPerpetratorFatalitiesConstraintValidator.java
@@ -0,0 +1,16 @@
+package com.nowakArtur97.globalTerrorismAPI.feature.victim;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+class ValidNumberOfPerpetratorFatalitiesConstraintValidator
+        implements ConstraintValidator<ValidNumberOfPerpetratorFatalities, Object> {
+
+    @Override
+    public boolean isValid(Object obj, ConstraintValidatorContext context) {
+
+        VictimDTO victim = (VictimDTO) obj;
+
+        return victim != null && victim.getTotalNumberOfFatalities() >= victim.getNumberOfPerpetratorFatalities();
+    }
+}
