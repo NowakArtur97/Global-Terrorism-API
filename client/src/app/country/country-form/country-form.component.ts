a1df3838551ce46a42d515af30f3edde4460db52
NowakArtur97 <nowart97@gmail.com>
2020-10-29 16:06:32 +0100

    Extend Abstract Form Component in Country Component

--- client/src/app/country/country-form/country-form.component.ts
+++ client/src/app/country/country-form/country-form.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, forwardRef } from '@angular/core';
+import { FormControl, FormGroup, NG_VALIDATORS, NG_VALUE_ACCESSOR, Validators } from '@angular/forms';
+import { AbstractFormComponent } from 'src/app/common/components/abstract-form.component';
 
 @Component({
   selector: 'app-country-form',
   templateUrl: './country-form.component.html',
-  styleUrls: ['./country-form.component.css']
+  styleUrls: ['./country-form.component.css'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => CountryFormComponent),
+      multi: true,
+    },
+    {
+      provide: NG_VALIDATORS,
+      useExisting: forwardRef(() => CountryFormComponent),
+      multi: true,
+    },
+  ],
 })
-export class CountryFormComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class CountryFormComponent extends AbstractFormComponent {
+  initForm(): void {
+    this.formGroup = new FormGroup({
+      name: new FormControl('', Validators.required),
+    });
   }
-
 }
