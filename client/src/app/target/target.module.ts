eb14ac228a504cbe651f68a251afd90d4791c32c
NowakArtur97 <nowart97@gmail.com>
2020-10-29 15:12:59 +0100

    Generate Region module

--- client/src/app/target/target.module.ts
+++ client/src/app/target/target.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+
 import { TargetFormComponent } from './target-form/target-form.component';
 
 @NgModule({
   declarations: [TargetFormComponent],
   imports: [CommonModule],
-  exports: [],
+  exports: [TargetFormComponent],
 })
 export class TargetModule {}
