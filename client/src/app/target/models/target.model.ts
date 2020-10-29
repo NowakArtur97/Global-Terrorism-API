9f1d3d0476b3e4e837d7f0fd85baf7aa5b85a75e
NowakArtur97 <nowart97@gmail.com>
2020-10-29 15:32:12 +0100

    Create Country model

--- client/src/app/target/models/target.model.ts
+++ client/src/app/target/models/target.model.ts
@@ -1,5 +1,11 @@
+import Country from 'src/app/country/models/country.model';
+
 export default class Target {
-  constructor(private readonly _id: number, private _target: string) {}
+  constructor(
+    private readonly _id: number,
+    private _target: string,
+    private _country: Country
+  ) {}
 
   public get id(): number {
     return this._id;
@@ -7,5 +13,9 @@ export default class Target {
 
   public get target(): string {
     return this._target;
+  }
+
+  public get country(): Country {
+    return this._country;
   }
 }
