4f7eb02e20ab0d69e06fdce3370ccbb3596ab41f
NowakArtur97 <nowart97@gmail.com>
2020-10-29 15:59:11 +0100

    Extend Abstract Form Component in Province Component

--- client/src/app/province/province-form/province-form.component.ts
+++ client/src/app/province/province-form/province-form.component.ts
@@ -1,12 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, forwardRef } from '@angular/core';
+import { FormControl, FormGroup, NG_VALIDATORS, NG_VALUE_ACCESSOR, Validators } from '@angular/forms';
+import { AbstractFormComponent } from 'src/app/common/components/abstract-form.component';
 
 @Component({
   selector: 'app-province-form',
   templateUrl: './province-form.component.html',
   styleUrls: ['./province-form.component.css'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => ProvinceFormComponent),
+      multi: true,
+    },
+    {
+      provide: NG_VALIDATORS,
+      useExisting: forwardRef(() => ProvinceFormComponent),
+      multi: true,
+    },
+  ],
 })
-export class ProvinceFormComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
+export class ProvinceFormComponent extends AbstractFormComponent {
+  initForm(): void {
+    this.formGroup = new FormGroup({
+      name: new FormControl('', Validators.required),
+    });
+  }
 }
