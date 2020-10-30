c91ce6adb01b7721086e1b1e400fa19e95a3ec6a
NowakArtur97 <nowart97@gmail.com>
2020-10-30 15:59:04 +0100

    Add tests in City Form component

--- client/src/app/city/city-form/city-form.component.spec.ts
+++ client/src/app/city/city-form/city-form.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { CityFormComponent } from './city-form.component';
 
@@ -8,18 +9,97 @@ describe('CityFormComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CityFormComponent ]
-    })
-    .compileComponents();
+      imports: [ReactiveFormsModule],
+      declarations: [CityFormComponent],
+    }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(CityFormComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    component.ngOnInit();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('form validation', () => {
+    it('with empty name should be invalid', () => {
+      component.name.setValue('');
+
+      const name = component.name;
+      const errors = name.errors;
+      expect(errors.notBlank).toBeTruthy();
+    });
+
+    it('with blank name should be invalid', () => {
+      component.name.setValue('    ');
+
+      const name = component.name;
+      const errors = name.errors;
+      expect(errors.notBlank).toBeTruthy();
+    });
+
+    it('with empty latitude should be invalid', () => {
+      component.latitude.setValue('');
+
+      const latitude = component.latitude;
+      const errors = latitude.errors;
+      expect(errors.notBlank).toBeTruthy();
+    });
+
+    it('with blank latitude should be invalid', () => {
+      component.latitude.setValue('    ');
+
+      const latitude = component.latitude;
+      const errors = latitude.errors;
+      expect(errors.notBlank).toBeTruthy();
+    });
+
+    it('with latitude lesser than -90 should be invalid', () => {
+      component.latitude.setValue(-91);
+
+      const latitude = component.latitude;
+      const errors = latitude.errors;
+      expect(errors.min).toBeTruthy();
+    });
+
+    it('with latitude bigger than 90 should be invalid', () => {
+      component.latitude.setValue(91);
+
+      const latitude = component.latitude;
+      const errors = latitude.errors;
+      expect(errors.max).toBeTruthy();
+    });
+
+    it('with empty longitude should be invalid', () => {
+      component.longitude.setValue('');
+
+      const longitude = component.longitude;
+      const errors = longitude.errors;
+      expect(errors.notBlank).toBeTruthy();
+    });
+
+    it('with blank longitude should be invalid', () => {
+      component.longitude.setValue('    ');
+
+      const longitude = component.longitude;
+      const errors = longitude.errors;
+      expect(errors.notBlank).toBeTruthy();
+    });
+
+    it('with longitude lesser than -190 should be invalid', () => {
+      component.longitude.setValue(-181);
+
+      const longitude = component.longitude;
+      const errors = longitude.errors;
+      expect(errors.min).toBeTruthy();
+    });
+
+    it('with longitude bigger than 180 should be invalid', () => {
+      component.longitude.setValue(181);
+
+      const longitude = component.longitude;
+      const errors = longitude.errors;
+      expect(errors.max).toBeTruthy();
+    });
   });
 });
