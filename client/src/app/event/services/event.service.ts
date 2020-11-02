eee58a95f04ab16e40e877b605b4a3e779333584
NowakArtur97 <nowart97@gmail.com>
2020-11-02 08:49:36 +0100

    Create add method in Event Service

--- client/src/app/event/services/event.service.ts
+++ client/src/app/event/services/event.service.ts
@@ -1,7 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import GenericRestService from 'src/app/common/services/generic-rest.service';
+import { environment } from 'src/environments/environment';
 
+import EventDTO from '../models/event.dto';
 import EventsGetResponse from '../models/events-get-response.model';
 
 @Injectable({ providedIn: 'root' })
@@ -10,5 +12,12 @@ export default class EventService extends GenericRestService<
 > {
   constructor(protected httpClient: HttpClient) {
     super(httpClient, 'events');
+  }
+
+  add(event: EventDTO): void {
+    this.httpClient.post<Event>(
+      `${environment.baseApiUrl}/${this.actionUtl}`,
+      event
+    );
   }
 }
