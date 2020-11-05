9e73c03f8d72c9d646f625f19699b6e068c11372
NowakArtur97 <nowart97@gmail.com>
2020-11-05 09:04:10 +0100

    Move Popup content to Marker Popup component

--- client/src/app/map/marker-popup/marker-popup.component.ts
+++ client/src/app/map/marker-popup/marker-popup.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+
+import Event from '../../event/models/event.model';
 
 @Component({
   selector: 'app-marker-popup',
   templateUrl: './marker-popup.component.html',
-  styleUrls: ['./marker-popup.component.css']
+  styleUrls: ['./marker-popup.component.css'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class MarkerPopupComponent implements OnInit {
+  @Input()
+  event: Event;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
