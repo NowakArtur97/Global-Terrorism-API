58a7a2f14194aee8b0e65955b97ccdf680e3cba3
NowakArtur97 <nowart97@gmail.com>
2020-11-05 18:16:31 +0100

    Select Event to Update in Province Form

--- client/src/app/province/province-form/province-form.component.ts
+++ client/src/app/province/province-form/province-form.component.ts
@@ -2,6 +2,9 @@ import { Component, forwardRef } from '@angular/core';
 import { AbstractControl, FormControl, FormGroup, NG_VALIDATORS, NG_VALUE_ACCESSOR } from '@angular/forms';
 import { AbstractFormComponent } from 'src/app/common/components/abstract-form.component';
 import CommonValidators from 'src/app/common/validators/common.validator';
+import { selectEventToUpdate } from 'src/app/event/store/event.reducer';
+
+import Event from '../../event/models//event.model';
 
 @Component({
   selector: 'app-province-form',
@@ -22,6 +25,15 @@ import CommonValidators from 'src/app/common/validators/common.validator';
 })
 export class ProvinceFormComponent extends AbstractFormComponent {
   initForm(): void {
+    let name = '';
+
+    this.store.select(selectEventToUpdate).subscribe((event: Event) => {
+      if (event) {
+        const province = event.city.province;
+        name = province.name;
+      }
+    });
+
     this.formGroup = new FormGroup({
       name: new FormControl('', [CommonValidators.notBlank]),
     });
