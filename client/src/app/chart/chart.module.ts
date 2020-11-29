0104c54413d8c88b6fc0ccc9f4c69d338709ba71
NowakArtur97 <nowart97@gmail.com>
2020-11-29 15:39:37 +0100

    Generate Angular Material dashboard component

--- client/src/app/chart/chart.module.ts
+++ client/src/app/chart/chart.module.ts
@@ -2,8 +2,17 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ChartsModule } from 'ng2-charts/lib/charts.module';
 
+import { MaterialModule } from '../common/material.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { LayoutModule } from '@angular/cdk/layout';
+
 @NgModule({
-  declarations: [],
-  imports: [CommonModule, ChartsModule],
+  declarations: [DashboardComponent],
+  imports: [CommonModule, ChartsModule, MaterialModule, MatGridListModule, MatCardModule, MatMenuModule, MatIconModule, MatButtonModule, LayoutModule],
 })
 export class ChartModule {}
