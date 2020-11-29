c5950d4f71771d9216dbb675ebc8c271ec5f6f0d
NowakArtur97 <nowart97@gmail.com>
2020-11-29 15:35:09 +0100

    Generate Chart module

---
+++ client/src/app/chart/chart.module.ts
@@ -0,0 +1,9 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ChartsModule } from 'ng2-charts/lib/charts.module';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, ChartsModule],
+})
+export class ChartModule {}
