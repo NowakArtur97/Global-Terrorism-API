6543e682256d24ccea598285d9df49e6e9642375
NowakArtur97 <nowart97@gmail.com>
2020-11-29 17:44:26 +0100

    Display data on Victim Chart component

--- client/src/app/chart/dashboard/dashboard.component.ts
+++ client/src/app/chart/dashboard/dashboard.component.ts
@@ -1,18 +1,13 @@
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Store } from '@ngrx/store';
-import { Subscription } from 'rxjs';
+import { Component } from '@angular/core';
 import { map } from 'rxjs/operators';
-import { selectAllEventsBeforeDate } from 'src/app/event/store/event.reducer';
-import AppStoreState from 'src/app/store/app.state';
-import Event from '../../event/models/event.model';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css'],
 })
-export class DashboardComponent implements OnInit, OnDestroy {
+export class DashboardComponent {
   /** Based on the screen size, switch from standard to one column per row */
   cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
     map(({ matches }) => {
@@ -23,24 +18,6 @@ export class DashboardComponent implements OnInit, OnDestroy {
       return [{ title: 'Card 1', cols: 2, rows: 2 }];
     })
   );
-  private eventsSubscription$: Subscription;
-  events: Event[] = [];
 
-  constructor(
-    private breakpointObserver: BreakpointObserver,
-    private store: Store<AppStoreState>
-  ) {}
-
-  ngOnInit(): void {
-    this.eventsSubscription$ = this.store
-      .select(selectAllEventsBeforeDate)
-      .subscribe((events: Event[]) => {
-        this.events = events;
-        console.log(events);
-      });
-  }
-
-  ngOnDestroy(): void {
-    this.eventsSubscription$?.unsubscribe();
-  }
+  constructor(private breakpointObserver: BreakpointObserver) {}
 }
--- client/src/app/chart/victims-chart/victims-chart.component.ts
+++ client/src/app/chart/victims-chart/victims-chart.component.ts
@@ -1,27 +1,65 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
 import { ChartOptions, ChartType } from 'chart.js';
 import { Label, SingleDataSet } from 'ng2-charts';
+import { Subscription } from 'rxjs';
+import { selectAllEventsBeforeDate } from 'src/app/event/store/event.reducer';
+import AppStoreState from 'src/app/store/app.state';
+
+import Event from '../../event/models/event.model';
 
 @Component({
   selector: 'app-victims-chart',
   templateUrl: './victims-chart.component.html',
   styleUrls: ['./victims-chart.component.css'],
 })
-export class VictimsChartComponent implements OnInit {
+export class VictimsChartComponent implements OnInit, OnDestroy {
   public pieChartOptions: ChartOptions = {
     responsive: true,
   };
   public pieChartLabels: Label[] = [
-    'Download Sales',
-    'In-Store Sales',
-    'Mail Sales',
+    'Total number of fatalities',
+    'Number of perpetrator fatalities',
+    'Total number of injured',
+    'Number of perpetrators injured',
   ];
-  public pieChartData: SingleDataSet = [300, 500, 100];
+
+  public pieChartData: SingleDataSet = [];
   public pieChartType: ChartType = 'pie';
   public pieChartLegend = true;
   public pieChartPlugins = [];
 
-  constructor() {}
+  private totalNumberOfFatalities = 0;
+  private numberOfPerpetratorFatalities = 0;
+  private totalNumberOfInjured = 0;
+  private numberOfPerpetratorInjured = 0;
 
-  ngOnInit() {}
+  private eventsSubscription$: Subscription;
+  events: Event[] = [];
+
+  constructor(private store: Store<AppStoreState>) {}
+
+  ngOnInit(): void {
+    this.eventsSubscription$ = this.store
+      .select(selectAllEventsBeforeDate)
+      .subscribe((events: Event[]) => {
+        this.events = events;
+        this.events.forEach(({ victim }) => {
+          this.totalNumberOfFatalities += victim.totalNumberOfFatalities;
+          this.numberOfPerpetratorFatalities +=
+            victim.numberOfPerpetratorFatalities;
+          this.totalNumberOfInjured += victim.totalNumberOfInjured;
+          this.numberOfPerpetratorInjured += victim.numberOfPerpetratorInjured;
+        });
+
+        this.pieChartData[0] = this.totalNumberOfFatalities;
+        this.pieChartData[1] = this.numberOfPerpetratorFatalities;
+        this.pieChartData[2] = this.totalNumberOfInjured;
+        this.pieChartData[3] = this.numberOfPerpetratorInjured;
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.eventsSubscription$?.unsubscribe();
+  }
 }
