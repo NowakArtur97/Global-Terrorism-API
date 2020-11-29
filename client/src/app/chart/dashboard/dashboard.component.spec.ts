b05496129c54d0e6d7f5a5e3012626e2c7512e57
NowakArtur97 <nowart97@gmail.com>
2020-11-29 17:57:51 +0100

    Refactor Dashboard and Victims Chart components tests

--- client/src/app/chart/dashboard/dashboard.component.spec.ts
+++ client/src/app/chart/dashboard/dashboard.component.spec.ts
@@ -1,19 +1,23 @@
 import { LayoutModule } from '@angular/cdk/layout';
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { Store, StoreModule } from '@ngrx/store';
+import { MaterialModule } from 'src/app/common/material.module';
+import AppStoreState from 'src/app/store/app.state';
 
 import { DashboardComponent } from './dashboard.component';
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
   let fixture: ComponentFixture<DashboardComponent>;
+  let store: Store<AppStoreState>;
 
-  beforeEach(async(() => {
+  beforeEach(async () => {
     TestBed.configureTestingModule({
       declarations: [DashboardComponent],
       imports: [
@@ -24,14 +28,20 @@ describe('DashboardComponent', () => {
         MatGridListModule,
         MatIconModule,
         MatMenuModule,
+
+        StoreModule.forRoot({}),
+        MaterialModule,
+        BrowserAnimationsModule,
       ],
+      providers: [Store],
     }).compileComponents();
-  }));
+  });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(DashboardComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+
+    store = TestBed.inject(Store);
   });
 
   it('should compile', () => {
--- client/src/app/chart/victims-chart/victims-chart.component.spec.ts
+++ client/src/app/chart/victims-chart/victims-chart.component.spec.ts
@@ -1,21 +1,33 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Store, StoreModule } from '@ngrx/store';
+import { MaterialModule } from 'src/app/common/material.module';
+import AppStoreState from 'src/app/store/app.state';
 
 import { VictimsChartComponent } from './victims-chart.component';
 
 describe('VictimsChartComponent', () => {
   let component: VictimsChartComponent;
   let fixture: ComponentFixture<VictimsChartComponent>;
+  let store: Store<AppStoreState>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [VictimsChartComponent],
+      imports: [
+        StoreModule.forRoot({}),
+        MaterialModule,
+        BrowserAnimationsModule,
+      ],
+      providers: [Store],
     }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(VictimsChartComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+
+    store = TestBed.inject(Store);
   });
 
   it('should create', () => {
