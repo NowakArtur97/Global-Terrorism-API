e5e49657370f93545dd13b5cebbb45751f524638
NowakArtur97 <nowart97@gmail.com>
2020-11-30 10:27:51 +0100

    Populate Events Over Years chart with data

--- client/src/app/charts-dashboard/charts/events-over-years-chart/events-over-years-chart.component.ts
+++ client/src/app/charts-dashboard/charts/events-over-years-chart/events-over-years-chart.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { ChartDataSets, ChartOptions, ChartType } from 'chart.js';
+import { Store } from '@ngrx/store';
+import { ChartDataSets, ChartOptions, ChartPoint, ChartType } from 'chart.js';
+import { Subscription } from 'rxjs';
+import { selectAllEvents } from 'src/app/event/store/event.reducer';
+import AppStoreState from 'src/app/store/app.state';
+
+import Event from '../../../event/models/event.model';
 
 @Component({
   selector: 'app-events-over-years-chart',
@@ -13,20 +19,40 @@ export class EventsOverYearsChartComponent implements OnInit {
 
   public scatterChartData: ChartDataSets[] = [
     {
-      data: [
-        { x: 1, y: 1 },
-        { x: 2, y: 3 },
-        { x: 3, y: -2 },
-        { x: 4, y: 4 },
-        { x: 5, y: -3, r: 20 },
-      ],
-      label: 'Series A',
+      data: [],
+      // data: [{ x: 1, y: 1 }],
+      label: 'Events over the years',
       pointRadius: 10,
     },
   ];
   public scatterChartType: ChartType = 'scatter';
 
-  constructor() {}
+  private eventsSubscription$: Subscription;
 
-  ngOnInit() {}
+  constructor(private store: Store<AppStoreState>) {}
+
+  ngOnInit(): void {
+    this.eventsSubscription$ = this.store
+      .select(selectAllEvents)
+      .subscribe((events: Event[]) => {
+        const actualYear = new Date().getFullYear();
+        let index = 0;
+        for (let year = 1970; year <= actualYear; year++) {
+          this.scatterChartData[0].data[index] = { x: year, y: 0 };
+          index++;
+        }
+
+        events.forEach(({ date }) => {
+          const year = new Date(date).getFullYear();
+          const eventsOverYear: ChartPoint = [
+            ...this.scatterChartData[0].data,
+          ].find((data: ChartPoint) => data.x === year);
+          eventsOverYear.y++;
+        });
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.eventsSubscription$?.unsubscribe();
+  }
 }
