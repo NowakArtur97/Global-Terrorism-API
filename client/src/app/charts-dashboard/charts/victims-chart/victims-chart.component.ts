01680ee4031cc15a0e2f510f8104536ccd20e0d5
NowakArtur97 <nowart97@gmail.com>
2020-12-01 08:54:22 +0100

    Make Victim Chart component an abstract component

--- client/src/app/charts-dashboard/charts/victims-chart/victims-chart.component.ts
+++ client/src/app/charts-dashboard/charts/victims-chart/victims-chart.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { ChartOptions, ChartType } from 'chart.js';
-import { Label, SingleDataSet } from 'ng2-charts';
+import { SingleDataSet } from 'ng2-charts';
 import { Subscription } from 'rxjs';
 import { selectAllEvents } from 'src/app/event/store/event.reducer';
 import AppStoreState from 'src/app/store/app.state';
@@ -13,28 +13,16 @@ import Event from '../../../event/models/event.model';
   templateUrl: './victims-chart.component.html',
   styleUrls: ['./victims-chart.component.css'],
 })
-export class VictimsChartComponent implements OnInit, OnDestroy {
-  public pieChartOptions: ChartOptions = {
+export abstract class VictimsChartComponent implements OnInit, OnDestroy {
+  pieChartOptions: ChartOptions = {
     responsive: true,
   };
-  public pieChartLabels: Label[] = [
-    'Total number of fatalities',
-    'Number of perpetrator fatalities',
-    'Total number of injured',
-    'Number of perpetrators injured',
-  ];
+  pieChartData: SingleDataSet = [];
+  pieChartType: ChartType = 'pie';
+  pieChartLegend = true;
+  pieChartPlugins = [];
 
-  public pieChartData: SingleDataSet = [];
-  public pieChartType: ChartType = 'pie';
-  public pieChartLegend = true;
-  public pieChartPlugins = [];
-
-  private totalNumberOfFatalities = 0;
-  private numberOfPerpetratorsFatalities = 0;
-  private totalNumberOfInjured = 0;
-  private numberOfPerpetratorsInjured = 0;
-
-  private eventsSubscription$: Subscription;
+  protected eventsSubscription$: Subscription;
 
   constructor(private store: Store<AppStoreState>) {}
 
@@ -42,28 +30,13 @@ export class VictimsChartComponent implements OnInit, OnDestroy {
     this.eventsSubscription$ = this.store
       .select(selectAllEvents)
       .subscribe((events: Event[]) => {
-        this.totalNumberOfFatalities = 0;
-        this.numberOfPerpetratorsFatalities = 0;
-        this.totalNumberOfInjured = 0;
-        this.numberOfPerpetratorsInjured = 0;
-
-        events.forEach(({ victim }) => {
-          this.totalNumberOfFatalities += victim.totalNumberOfFatalities;
-          this.numberOfPerpetratorsFatalities +=
-            victim.numberOfPerpetratorsFatalities;
-          this.totalNumberOfInjured += victim.totalNumberOfInjured;
-          this.numberOfPerpetratorsInjured +=
-            victim.numberOfPerpetratorsInjured;
-        });
-
-        this.pieChartData[0] = this.totalNumberOfFatalities;
-        this.pieChartData[1] = this.numberOfPerpetratorsFatalities;
-        this.pieChartData[2] = this.totalNumberOfInjured;
-        this.pieChartData[3] = this.numberOfPerpetratorsInjured;
+        this.populateChart(events);
       });
   }
 
   ngOnDestroy(): void {
     this.eventsSubscription$?.unsubscribe();
   }
+
+  protected abstract populateChart(events: Event[]): void;
 }
