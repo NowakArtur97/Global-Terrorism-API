22d680920d1b67c4185926943604dfad0fc61739
NowakArtur97 <nowart97@gmail.com>
2020-12-01 08:48:39 +0100

    Generate Fatalities Chart component

--- client/src/app/charts-dashboard/charts-dashboard.module.ts
+++ client/src/app/charts-dashboard/charts-dashboard.module.ts
@@ -13,12 +13,14 @@ import ChartsDashboardRoutingModule from './charts-dashboard-routing.module';
 import { EventsOverYearsChartComponent } from './charts/events-over-years-chart/events-over-years-chart.component';
 import { VictimsChartComponent } from './charts/victims-chart/victims-chart.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { FatalitiesChartComponent } from './charts/victims-chart/fatalities-chart/fatalities-chart.component';
 
 @NgModule({
   declarations: [
     DashboardComponent,
     VictimsChartComponent,
     EventsOverYearsChartComponent,
+    FatalitiesChartComponent,
   ],
   imports: [
     MatGridListModule,
---
+++ client/src/app/charts-dashboard/charts/victims-chart/fatalities-chart/fatalities-chart.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fatalities-chart',
+  templateUrl: './fatalities-chart.component.html',
+  styleUrls: ['./fatalities-chart.component.css']
+})
+export class FatalitiesChartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
