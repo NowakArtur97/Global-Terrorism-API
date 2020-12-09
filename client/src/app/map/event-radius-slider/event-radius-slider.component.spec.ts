152b5777b4944546966f7808872a044d4d7ab88b
NowakArtur97 <nowart97@gmail.com>
2020-12-09 18:26:29 +0100

    Generate Event Radius Slider component

---
+++ client/src/app/map/event-radius-slider/event-radius-slider.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { EventRadiusSliderComponent } from './event-radius-slider.component';
+
+describe('EventRadiusSliderComponent', () => {
+  let component: EventRadiusSliderComponent;
+  let fixture: ComponentFixture<EventRadiusSliderComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ EventRadiusSliderComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(EventRadiusSliderComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ client/src/app/map/event-radius-slider/event-radius-slider.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-event-radius-slider',
+  templateUrl: './event-radius-slider.component.html',
+  styleUrls: ['./event-radius-slider.component.css']
+})
+export class EventRadiusSliderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
