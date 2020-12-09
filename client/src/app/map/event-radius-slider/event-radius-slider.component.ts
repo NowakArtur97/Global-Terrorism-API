d2ceb173c4183a8f1f511e7c6c1f74a2ff95ae3f
NowakArtur97 <nowart97@gmail.com>
2020-12-09 19:06:12 +0100

    Display Event Radius Slider component

--- client/src/app/map/event-radius-slider/event-radius-slider.component.ts
+++ client/src/app/map/event-radius-slider/event-radius-slider.component.ts
@@ -3,13 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-event-radius-slider',
   templateUrl: './event-radius-slider.component.html',
-  styleUrls: ['./event-radius-slider.component.css']
+  styleUrls: ['./event-radius-slider.component.css'],
 })
 export class EventRadiusSliderComponent implements OnInit {
+  readonly maxRadius = 20;
+  value = 0;
+  // constructor(private store: Store<AppStoreState>) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onRadiusChange(): void {
+    console.log(this.value);
   }
-
 }
