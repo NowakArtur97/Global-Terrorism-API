3b3984ec74e6db077c1e9bbd74f854527c5266d0
NowakArtur97 <nowart97@gmail.com>
2020-12-28 15:10:59 +0100

    Populate autocomplete element with cities

--- client/src/app/city/city-form/city-form.component.ts
+++ client/src/app/city/city-form/city-form.component.ts
@@ -1,10 +1,16 @@
-import { Component, forwardRef } from '@angular/core';
+import { Component, forwardRef, OnDestroy, OnInit } from '@angular/core';
 import { AbstractControl, FormControl, FormGroup, NG_VALIDATORS, NG_VALUE_ACCESSOR, Validators } from '@angular/forms';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
+import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs';
 import { AbstractFormComponent } from 'src/app/common/components/abstract-form.component';
 import CommonValidators from 'src/app/common/validators/common.validator';
 import { selectEventToUpdate } from 'src/app/event/store/event.reducer';
+import AppStoreState from 'src/app/store/app.state';
 
 import Event from '../../event/models//event.model';
+import City from '../models/city.model';
+import CityService from '../services/city.service';
 
 @Component({
   selector: 'app-city-form',
@@ -23,7 +29,30 @@ import Event from '../../event/models//event.model';
     },
   ],
 })
-export class CityFormComponent extends AbstractFormComponent {
+export class CityFormComponent
+  extends AbstractFormComponent
+  implements OnInit, OnDestroy {
+  private citiesSubscription: Subscription;
+  cities: City[] = [];
+
+  constructor(
+    protected store: Store<AppStoreState>,
+    private cityService: CityService
+  ) {
+    super(store);
+  }
+  ngOnInit(): void {
+    super.ngOnInit();
+    this.citiesSubscription = this.cityService
+      .getAll()
+      .subscribe((citiesResponse) => (this.cities = citiesResponse.content));
+  }
+
+  ngOnDestroy(): void {
+    super.ngOnDestroy();
+    this.citiesSubscription?.unsubscribe();
+  }
+
   initForm(): void {
     let name = '';
     let latitude = 0;
@@ -65,5 +94,17 @@ export class CityFormComponent extends AbstractFormComponent {
 
   get longitude(): AbstractControl {
     return this.formGroup.get('longitude');
+  }
+
+  selectCity(event: MatAutocompleteSelectedEvent): void {
+    if (!event.option) {
+      return;
+    }
+    const cityName = event.option.value;
+    this.name.setValue(cityName);
+
+    const city = this.cities.find((city) => city.name === cityName);
+    this.latitude.setValue(city.latitude);
+    this.longitude.setValue(city.longitude);
   }
 }
