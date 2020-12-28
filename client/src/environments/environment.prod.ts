f82e8edc66c599e77957388db21e322e6a64e48e
NowakArtur97 <nowart97@gmail.com>
2020-12-28 18:35:09 +0100

    Add bulk request url variable to all environments

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
   baseApiUrl: 'https://global-terrorism-api.herokuapp.com/api/v1',
+  bulkRequestUrl: 'bulk',
 };
--- client/src/environments/environment.testing.ts
+++ client/src/environments/environment.testing.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
   baseApiUrl: 'http://localhost:8080/api/v1',
+  bulkRequestUrl: 'bulk',
 };
